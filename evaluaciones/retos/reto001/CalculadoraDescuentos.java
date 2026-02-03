a71687fb2dd39b1cc9bf16975fb1df00281c7828
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2026-02-03 12:22:12 +0100

    introduccion de la clase MAX_PRO ya que es un valor que usa todo el programa y evita numeros magicos

--- evaluaciones/retos/reto001/CalculadoraDescuentos.java
+++ evaluaciones/retos/reto001/CalculadoraDescuentos.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class CalculadoraDescuentos {
+
+    static final int MAX_PROD = 10;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
