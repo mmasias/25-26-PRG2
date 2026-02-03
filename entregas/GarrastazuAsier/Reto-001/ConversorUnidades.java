beac1111bf2d1de878797d5a962dee0937614eb3
Asier Garrastazu <asier.gc20072gmail.com>
2026-02-03 14:08:16 +0100

    cambio el nombre de "v" a "valor" el resto de variables no las cambio porque se entienden mejor por las abreviaturas q poniendo el nombre entero

--- entregas/GarrastazuAsier/Reto-001/ConversorUnidades.java
+++ entregas/GarrastazuAsier/Reto-001/ConversorUnidades.java
@@ -37,49 +37,49 @@ public class ConversorUnidades {
         System.out.println("pulgadas_centimetros");
         System.out.print("Opcion: ");
 
-        String op = sc.next().toLowerCase();
+        String opcion = sc.next().toLowerCase();
         System.out.print("Valor: ");
-        double v = sc.nextDouble();
+        double valor = sc.nextDouble();
 
-        if (op.equals("metros_kilometros")) {
-            metrosAKilometros(v);
-        } else if (op.equals("kilometros_metros")) {
-            kilometrosAMetros(v);
-        } else if (op.equals("metros_millas")) {
-            metrosAMillas(v);
-        } else if (op.equals("millas_metros")) {
-            millasAMetros(v);
-        } else if (op.equals("centimetros_pulgadas")) {
-            centimetrosAPulgadas(v);
-        } else if (op.equals("pulgadas_centimetros")) {
-            pulgadasACentimetros(v);
+        if (opcion.equals("metros_kilometros")) {
+            metrosAKilometros(valor);
+        } else if (opcion.equals("kilometros_metros")) {
+            kilometrosAMetros(valor);
+        } else if (opcion.equals("metros_millas")) {
+            metrosAMillas(valor);
+        } else if (opcion.equals("millas_metros")) {
+            millasAMetros(valor);
+        } else if (opcion.equals("centimetros_pulgadas")) {
+            centimetrosAPulgadas(valor);
+        } else if (opcion.equals("pulgadas_centimetros")) {
+            pulgadasACentimetros(valor);
         } else {
             System.out.println("Opcion no valida");
         }
     }
 
-    static void metrosAKilometros(double v) {
-        System.out.println(v + " m = " + (v / 1000) + " km");
+    static void metrosAKilometros(double valor) {
+        System.out.println(valor + " m = " + (valor / 1000) + " km");
     }
 
-    static void kilometrosAMetros(double v) {
-        System.out.println(v + " km = " + (v * 1000) + " m");
+    static void kilometrosAMetros(double valor) {
+        System.out.println(valor + " km = " + (valor * 1000) + " m");
     }
 
-    static void metrosAMillas(double v) {
-        System.out.println(v + " m = " + (v * 0.000621371) + " mi");
+    static void metrosAMillas(double valor) {
+        System.out.println(valor + " m = " + (valor * 0.000621371) + " mi");
     }
 
-    static void millasAMetros(double v) {
-        System.out.println(v + " mi = " + (v * 1609.34) + " m");
+    static void millasAMetros(double valor) {
+        System.out.println(valor + " mi = " + (valor * 1609.34) + " m");
     }
 
-    static void centimetrosAPulgadas(double v) {
-        System.out.println(v + " cm = " + (v * 0.393701) + " in");
+    static void centimetrosAPulgadas(double valor) {
+        System.out.println(valor + " cm = " + (valor * 0.393701) + " in");
     }
 
-    static void pulgadasACentimetros(double v) {
-        System.out.println(v + " in = " + (v * 2.54) + " cm");
+    static void pulgadasACentimetros(double valor) {
+        System.out.println(valor + " in = " + (valor * 2.54) + " cm");
     }
 
     static void menuPeso() {
@@ -92,49 +92,49 @@ public class ConversorUnidades {
         System.out.println("oz_g");
         System.out.print("Opcion: ");
 
-        String op = sc.next().toLowerCase();
+        String opcion = sc.next().toLowerCase();
         System.out.print("Valor: ");
-        double v = sc.nextDouble();
+        double valor = sc.nextDouble();
 
-        if (op.equals("kg_g")) {
-            kgAG(v);
-        } else if (op.equals("g_kg")) {
-            gAKg(v);
-        } else if (op.equals("kg_lb")) {
-            kgALb(v);
-        } else if (op.equals("lb_kg")) {
-            lbAKg(v);
-        } else if (op.equals("g_oz")) {
-            gAOz(v);
-        } else if (op.equals("oz_g")) {
-            ozAG(v);
+        if (opcion.equals("kg_g")) {
+            kgAG(valor);
+        } else if (opcion.equals("g_kg")) {
+            gAKg(valor);
+        } else if (opcion.equals("kg_lb")) {
+            kgALb(valor);
+        } else if (opcion.equals("lb_kg")) {
+            lbAKg(valor);
+        } else if (opcion.equals("g_oz")) {
+            gAOz(valor);
+        } else if (opcion.equals("oz_g")) {
+            ozAG(valor);
         } else {
             System.out.println("Opcion no valida");
         }
     }
 
-    static void kgAG(double v) {
-        System.out.println(v + " kg = " + (v * 1000) + " g");
+    static void kgAG(double valor) {
+        System.out.println(valor + " kg = " + (valor * 1000) + " g");
     }
 
-    static void gAKg(double v) {
-        System.out.println(v + " g = " + (v / 1000) + " kg");
+    static void gAKg(double valor) {
+        System.out.println(valor + " g = " + (valor / 1000) + " kg");
     }
 
-    static void kgALb(double v) {
-        System.out.println(v + " kg = " + (v * 2.20462) + " lb");
+    static void kgALb(double valor) {
+        System.out.println(valor + " kg = " + (valor * 2.20462) + " lb");
     }
 
-    static void lbAKg(double v) {
-        System.out.println(v + " lb = " + (v * 0.453592) + " kg");
+    static void lbAKg(double valor) {
+        System.out.println(valor + " lb = " + (valor * 0.453592) + " kg");
     }
 
-    static void gAOz(double v) {
-        System.out.println(v + " g = " + (v * 0.035274) + " oz");
+    static void gAOz(double valor) {
+        System.out.println(valor + " g = " + (valor * 0.035274) + " oz");
     }
 
-    static void ozAG(double v) {
-        System.out.println(v + " oz = " + (v * 28.3495) + " g");
+    static void ozAG(double valor) {
+        System.out.println(valor + " oz = " + (valor * 28.3495) + " g");
     }
 
     static void menuTemperatura() {
@@ -145,36 +145,36 @@ public class ConversorUnidades {
         System.out.println("k_c");
         System.out.print("Opcion: ");
 
-        String op = sc.next().toLowerCase();
+        String opcion = sc.next().toLowerCase();
         System.out.print("Valor: ");
-        double v = sc.nextDouble();
+        double valor = sc.nextDouble();
 
-        if (op.equals("c_f")) {
-            cAF(v);
-        } else if (op.equals("f_c")) {
-            fAC(v);
-        } else if (op.equals("c_k")) {
-            cAK(v);
-        } else if (op.equals("k_c")) {
-            kAC(v);
+        if (opcion.equals("c_f")) {
+            cAF(valor);
+        } else if (opcion.equals("f_c")) {
+            fAC(valor);
+        } else if (opcion.equals("c_k")) {
+            cAK(valor);
+        } else if (opcion.equals("k_c")) {
+            kAC(valor);
         } else {
             System.out.println("Opcion no valida");
         }
     }
 
-    static void cAF(double v) {
-        System.out.println(v + " C = " + (v * 9 / 5 + 32) + " F");
+    static void cAF(double valor) {
+        System.out.println(valor + " C = " + (valor * 9 / 5 + 32) + " F");
     }
 
-    static void fAC(double v) {
-        System.out.println(v + " F = " + ((v - 32) * 5 / 9) + " C");
+    static void fAC(double valor) {
+        System.out.println(valor + " F = " + ((valor - 32) * 5 / 9) + " C");
     }
 
-    static void cAK(double v) {
-        System.out.println(v + " C = " + (v + 273.15) + " K");
+    static void cAK(double valor) {
+        System.out.println(valor + " C = " + (valor + 273.15) + " K");
     }
 
-    static void kAC(double v) {
-        System.out.println(v + " K = " + (v - 273.15) + " C");
+    static void kAC(double valor) {
+        System.out.println(valor + " K = " + (valor - 273.15) + " C");
     }
 }
