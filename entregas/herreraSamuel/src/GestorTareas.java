61f2be15a4166d1b9c7f65ea44d204d1ec4cb877
Samuel Alejandro Herrera Castillo <samuel.herrera@unealum.local>
2026-02-03 14:10:38 +0100

    Refactor: usar constante MAX_TAREAS para evitar números mágicos

--- entregas/herreraSamuel/src/GestorTareas.java
+++ entregas/herreraSamuel/src/GestorTareas.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class GestorTareas {
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tareas = new String[10];
