a1e04c6002a5d1f1703e93d3469e902290289204
katerine <katerine.rafael@alumnos.uneatlantico.es>
2026-02-06 11:30:04 +0100

    Interfaz y mejoras

--- entregas/rafaelKaterine/src/JEdlin.java
+++ entregas/rafaelKaterine/src/JEdlin.java
@@ -2,27 +2,38 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class JEdlin {
-    private String[] lines;
-    private int activateLine;
+    private String[] linea;
+    private int lineaSeleccionada;
     private Scanner scanner;
 
     public JEdlin() {
-        lines = new String[10];
-        Arrays.fill(lines,"");
-        lines[0] = "Bienvenidos al editor EDLIN";
-        lines[1]= "Utilice el menu inferior para editar el texto";
-        lines[2]= "---------------------------------------------";
-        activateLine = 0;
+        linea = new String[10];
+        Arrays.fill(linea,"");
+        linea[0] = "Bienvenidos al editor EDLIN";
+        linea[1]= "Utilice el menu inferior para editar el texto";
+        linea[2]= "---------------------------------------------";
+        lineaSeleccionada = 0;
         scanner = new Scanner(System.in)
     }
 
-    private void clearScreen(){
+    private void limpiarPantalla(){
         System.out.print("/////////");
         System.out.flush();
         for (int i = 0; i < 50; i++) System.out.println();
     }
 
-    
+    public void interfaz(){
+        limpiarPantalla();
+        for (int i = 0; i < linea.length; i++){
+            String marker = (i == lineaSeleccionada) ? "*": "";
+            System.out.printf("%d: %s| %s%n", i, marker, linea[i]);
+        }
+    }
+
+    private void setActivateLine(){
+        
+    }
+
 }
 
 
