189c6d75aba5e792fbe70e4ef6325150a4f631da
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-02-06 11:34:47 +0100

    Subida Reto-002

---
+++ entregas/ArroyoSanchezDaniel/reto-002/Reto002.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Reto002 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean salirPrograma = false;
+        int lineaActiva = 0;
+        String[] filasEditables = { "", "", "", "", "", "", "", "", "", "" };
+
+        do {
+
+            MostrarPantalla(filasEditables, lineaActiva);
+            String opcionUsuario;
+            int nuevaLinea;
+            System.out.println("¿Qué quiere hacer?");
+            opcionUsuario = scanner.nextLine().toUpperCase();
+
+            switch (opcionUsuario) {
+                case "L":
+                    System.out.print("Introduzca número de línea (0-9): ");
+                    nuevaLinea = scanner.nextInt();
+                    if (nuevaLinea >= 0 && nuevaLinea <= 9) {
+                        lineaActiva = nuevaLinea;
+                    } else {
+                        System.out.println("Línea fuera de rango.");
+                    }
+                    break;
+
+                case "E":
+                    System.out.print("Editando línea " + lineaActiva + ". Nuevo texto: ");
+                    filasEditables[lineaActiva] = scanner.nextLine();
+                    break;
+
+                case "I":
+                    System.out.println("¿Qué línea desea intercambiar con la línea " + lineaActiva + "? (0-9)");
+                    boolean líneasCambiadas = false;
+                    while (!líneasCambiadas) {
+                        if (lineaActiva >= 0 && lineaActiva <= 9) {
+                            int lineaCambio = scanner.nextInt();
+                            String temporal = filasEditables[lineaActiva];
+                            filasEditables[lineaActiva] = filasEditables[lineaCambio];
+                            filasEditables[lineaCambio] = temporal;
+                            System.out.println("Cambio realizado!");
+                        }
+                    }
+                    break;
+
+                case "S":
+                    System.out.println("Saliendo del programa");
+                    salirPrograma = true;
+                    break;
+
+                case "B":
+                    filasEditables[lineaActiva] = "";
+                    System.out.println("Línea borrada.");
+                    break;
+
+                default:
+                    System.out.println("Comando no reconocido.");
+                    break;
+
+            }
+            System.out.println();
+        } while (!salirPrograma);
+
+    }
+
+    static void MostrarPantalla(String[] filasEditables, int lineaActiva) {
+        String linea = "--------------------------------------------------";
+        System.out.println(linea);
+        for (int i = 0; i < 10; i++) {
+            if (lineaActiva == i) {
+                System.out.println(i + ":*| " + filasEditables[i]);
+            } else {
+                System.out.println(i + ": | " + filasEditables[i]);
+            }
+        }
+        System.out.println(linea);
+        System.out.println("Comandos: [L]inea Activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir ");
+        System.out.println();
+
+    }
+
+}
