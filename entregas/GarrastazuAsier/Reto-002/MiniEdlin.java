7058dc0134fb3d43b0b646d55f68ba5672eeadc9
Asier Garrastazu <asier.gc20072gmail.com>
2026-02-06 11:30:27 +0100

    Movemos lógica a métodos

--- entregas/GarrastazuAsier/Reto-002/MiniEdlin.java
+++ entregas/GarrastazuAsier/Reto-002/MiniEdlin.java
@@ -2,60 +2,64 @@ import java.util.Scanner;
 
 public class MiniEdlin {
 
+    static String[] texto = new String[10];
+    static int lineaActiva = 0;
+
     public static void main(String[] args) {
 
-        Scanner teclado = new Scanner(System.in);
-
-        String[] texto = new String[10];
-        int lineaActiva = 0;
-        int opcion = 0;
+        Scanner sc = new Scanner(System.in);
 
         for (int i = 0; i < 10; i++) texto[i] = "";
 
+        int opcion;
+
         do {
+            menu();
+            opcion = sc.nextInt();
+            sc.nextLine();
 
-            System.out.println("1 mostrar");
-            System.out.println("2 activa");
-            System.out.println("3 editar");
-            System.out.println("4 borrar");
-            System.out.println("5 salir");
-
-            opcion = teclado.nextInt();
-            teclado.nextLine();
-
-            if (opcion == 1) {
-
-                for (int i = 0; i < 10; i++) {
-                    if (i == lineaActiva)
-                        System.out.println(">" + (i + 1) + ":" + texto[i]);
-                    else
-                        System.out.println(" " + (i + 1) + ":" + texto[i]);
-                }
-            }
-
-            else if (opcion == 2) {
-
-                int nueva = teclado.nextInt();
-                teclado.nextLine();
-
-                if (nueva > 0 && nueva <= 10)
-                    lineaActiva = nueva - 1;
-            }
-
-            else if (opcion == 3) {
-
-                texto[lineaActiva] = teclado.nextLine();
-            }
-
-            else if (opcion == 4) {
-
-                int borrar = teclado.nextInt();
-                teclado.nextLine();
-
-                if (borrar > 0 && borrar <= 10)
-                    texto[borrar - 1] = "";
-            }
+            if (opcion == 1) mostrar();
+            else if (opcion == 2) cambiar(sc);
+            else if (opcion == 3) editar(sc);
+            else if (opcion == 4) borrar(sc);
 
         } while (opcion != 5);
+    }
+
+    static void menu() {
+        System.out.println("1 mostrar");
+        System.out.println("2 activa");
+        System.out.println("3 editar");
+        System.out.println("4 borrar");
+        System.out.println("5 salir");
+    }
+
+    static void mostrar() {
+        for (int i = 0; i < 10; i++) {
+            if (i == lineaActiva)
+                System.out.println(">" + (i + 1) + ":" + texto[i]);
+            else
+                System.out.println(" " + (i + 1) + ":" + texto[i]);
+        }
+    }
+
+    static void cambiar(Scanner sc) {
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        if (n > 0 && n <= 10)
+            lineaActiva = n - 1;
+    }
+
+    static void editar(Scanner sc) {
+        texto[lineaActiva] = sc.nextLine();
+    }
+
+    static void borrar(Scanner sc) {
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        if (n > 0 && n <= 10)
+            texto[n - 1] = "";
     }
 }
