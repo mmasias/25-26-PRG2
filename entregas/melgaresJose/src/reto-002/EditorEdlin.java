8b73b8a1c8be91e945203e5e4cd338ffbeb9b45b
Jose Melgares <rjmzelaya@gmail.com>
2026-02-09 18:26:17 +0100

    feat: imprime menu inicial y mapea la el comando para la navegacion

--- entregas/melgaresJose/src/reto-002/EditorEdlin.java
+++ entregas/melgaresJose/src/reto-002/EditorEdlin.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 class EditorEdlin {
     public static void main(String[] args) {
-        System.out.println("EDLIN v1");
+        int opcion;
+        imprimirMenuIncial();
+        opcion = leerComando();
+        boolean opcionValida = opcion > -1;
+        System.out.println(opcionValida ? "EDLIN v1" : "WRONGGG");
+    }
+
+    private static void imprimirMenuIncial() {
+        System.out.println("--------------------------------------------------");
+        System.out.println("0: | Bienvenidos al editor EDLIN");
+        System.out.println("1:*| Utilice el menu inferior para editar el texto");
+        System.out.println("2: | ------");
+        System.out.println("3: | [L] permite definir la linea activa");
+        System.out.println("4: | [E] permite editar la linea activa");
+        System.out.println("5: | [I] permite intercambiar dos lineas");
+        System.out.println("6: | [B] borra el contenido de la linea activa");
+        System.out.println("7: | [S] sale del programa");
+        System.out.println("8: | ");
+        System.out.println("9: | ");
+        System.out.println("--------------------------------------------------");
+        imprimirBarraEstado();
+    }
+
+    private static void imprimirBarraEstado() {
+        System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir");
+    }
+
+    private static int leerComando() {
+        Scanner scanner = new Scanner(System.in);
+        return mapearComando(scanner.nextLine());
+    }
+
+    private static int mapearComando(String comando) {
+        String[] comandosValidos = { "L", "E", "I", "B", "S" };
+        final int OPCION_INVALIDA = -1;
+
+        for (int i = 0; i < comandosValidos.length; i++) {
+            if (comando.equalsIgnoreCase(comandosValidos[i]))
+                return i;
+        }
+        return OPCION_INVALIDA;
     }
 }
