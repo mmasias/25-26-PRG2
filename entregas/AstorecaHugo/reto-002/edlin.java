789baa0cb5d29775d6dbeac7193045fd0104996c
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-02-10 13:41:00 +0100

    creo el comando para intercambiar lineas y lo pongo en el switch

--- entregas/AstorecaHugo/reto-002/edlin.java
+++ entregas/AstorecaHugo/reto-002/edlin.java
@@ -3,32 +3,35 @@ import java.util.Scanner;
 
 class edlin {
 
-    private static final int MAX_LINEAS = 100;
+    private static final int MAX_LINEAS = 10;
     private static String[] lineas = new String[MAX_LINEAS];
     private static int lineaActual = 0;
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-            cargarTexto();
-            boolean salir = false;
-            while(!salir){
-                mostrarMenu();
-                System.out.print("Ingrese un comando: ");
-                String comando = scanner.nextLine();
-                switch (comando){
-                    case"L":
+        cargarTexto();
+        boolean salir = false;
+        while (!salir) {
+            mostrarMenu();
+            System.out.print("Ingrese un comando: ");
+            String comando = scanner.nextLine();
+            switch (comando) {
+                case "L":
                     cambiarLineaActiva();
                     break;
-                    case "E":
-                        editarLineaActiva();
-                        break;
-                        case"B":
-                        borrarLineaActiva();
-                        break;
-
-                }
+                case "E":
+                    editarLineaActiva();
+                    break;
+                case "B":
+                    borrarLineaActiva();
+                    break;
+                case "I":
+                    intercambiarLineas();
+                    break;
 
             }
+
+        }
     }
 
     static void cargarTexto() {
@@ -46,7 +49,7 @@ class edlin {
         lineas[7] = "[S] sale del programa";
     }
 
-    static void mostrarMenu(){
+    static void mostrarMenu() {
         System.out.println("--------------------------------------------------");
         for (int i = 0; i < MAX_LINEAS; i++) {
             String seleccion;
@@ -61,32 +64,39 @@ class edlin {
         System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir");
     }
 
-static void cambiarLineaActiva(){
-     System.out.print("Número de línea (0-9): ");
+    static void cambiarLineaActiva() {
+        System.out.print("Número de línea (0-9): ");
         int nueva = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
 
         if (nueva >= 0 && nueva < MAX_LINEAS) {
             lineaActual = nueva;
         }
     }
 
-static void editarLineaActiva(){
-    System.out.print("Nuevo contenido: ");
+    static void editarLineaActiva() {
+        System.out.print("Nuevo contenido: ");
         lineas[lineaActual] = scanner.nextLine();
+    }
+
+    static void borrarLineaActiva() {
+        lineas[lineaActual] = "";
+
+    }
+
+    static void intercambiarLineas() {
+        System.out.println("Numero de la linea que quiere intercabiar(0-9)");
+        int linea1 = scanner.nextInt();
+        System.out.println("Numero de la linea que quiere intercabiar(0-9)");
+        int linea2 = scanner.nextInt();
+        scanner.nextLine();
+        if (linea1 >= 0 && linea1 < MAX_LINEAS && linea2 >= 0 && linea2 < MAX_LINEAS) {
+            String contenidolinea1 = lineas[linea1];
+            lineas[linea1] = lineas[linea2];
+            lineas[linea2] = contenidolinea1;
+
+        }
+
+    }
+
 }
-
-static void borrarLineaActiva(){
-    lineas[lineaActual]= "";
-            
-}
-
-
-
-
-
-}
-
-
-
-
