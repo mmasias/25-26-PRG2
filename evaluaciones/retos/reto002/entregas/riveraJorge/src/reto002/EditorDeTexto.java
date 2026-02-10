3efe96392f600f0eeb678bbe4f9bec55fa447d6e
jerb05 <jerb0508@gmail.com>
2026-02-10 13:41:23 +0100

    InicializacionDelEditor Se inicializa el editor poniendo cuantas lineas tiene y que mientras se ejecuta se muestra los comandos.

---
+++ evaluaciones/retos/reto002/entregas/riveraJorge/src/reto002/EditorDeTexto.java
@@ -0,0 +1,32 @@
+package evaluaciones.retos.reto002.entregas.riveraJorge.src.reto002;
+
+import java.util.Scanner;
+
+public class EditorDeTexto {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int MAXIMO_LINEAS = 10;
+        String[] lineas = new String[MAXIMO_LINEAS];
+        for (int i = 0; i < MAXIMO_LINEAS; i++) {
+            lineas[i] = "";
+        }
+
+        String[] copia = new String[MAXIMO_LINEAS];
+
+        int lineaActiva = 0;
+        
+        boolean ejecutar = true;
+
+        while (ejecutar) {
+            System.out.println("comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [D]eshacer | [S]alir");
+            
+                
+            }
+           
+        }
+
+    }
+    
+
