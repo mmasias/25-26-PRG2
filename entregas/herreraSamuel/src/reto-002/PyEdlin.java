506a281d9dd63d146d2d92e025a6a4d10d339b43
Samuel Alejandro Herrera Castillo <samuel.herrera@unealum.local>
2026-02-10 13:37:04 +0100

    Inicialización de variables, carga de contenido predeterminado y respaldo inicial en el historial.

--- entregas/herreraSamuel/src/reto-002/PyEdlin.java
+++ entregas/herreraSamuel/src/reto-002/PyEdlin.java
@@ -9,8 +9,21 @@ public class PyEdlin {
         String[] historial = new String[10];
         int LineaActiva = 0;
 
+        lineasDeTexto[0] = "Bienvenidos al editor EDLIN";
+        lineasDeTexto[1] = "Utilice el menu inferior para editar el texto";
+        lineasDeTexto[2] = "------";
+        lineasDeTexto[3] = "[L] permite definir la linea activa";
+        lineasDeTexto[4] = "[E] permite editar la linea activa";
+        lineasDeTexto[5] = "[I] permite intercambiar dos lineas";
+        lineasDeTexto[6] = "[B] borra el contenido de la linea activa";
+        lineasDeTexto[7] = "[D] deshace la última acción realizada";
+        lineasDeTexto[8] = "[S] sale del programa";
+        lineasDeTexto[9] = "--------------------------------------------------";
+
+        for (int i = 0; i < 10; i++) {
+            historial[i] = lineasDeTexto[i];
+        }
+
     }
 
 }
-
-
