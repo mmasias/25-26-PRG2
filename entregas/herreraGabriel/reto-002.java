7e6746f4e4a4b0b37f03d2694b69bdce040cebb0
Gabriel19-87 <gabriel.herrera@alumnos.uneatlantico.es>
2026-02-10 13:20:46 +0100

    agregado mejora 1

--- entregas/herreraGabriel/reto-002.java
+++ entregas/herreraGabriel/reto-002.java
@@ -1,75 +1,102 @@
 import java.util.Scanner;
 
-class reto-002 {
+class PyEdlin {
 
     public static void main(String[] args) {
 
-        String[] documento = crearDocumento();
-        int lineaActiva = 0;
-        final int COMANDO_SALIR = 0;
+        String[] documento = obtenerDocumentoInicial();
+        String[] backup = documento.clone();
+        int lineaActiva = 1;
+        boolean ejecutar = true;
 
-        int opcion;
         do {
-            imprimirEditor(documento, lineaActiva);
-            opcion = solicitarOpcion();
-            
-            if (opcion == 1) {
-                lineaActiva = seleccionarLinea(documento.length);
-            } else if (opcion == 2) {
-                editarLinea(documento, lineaActiva);
-            } else if (opcion == 3) {
-                eliminarContenido(documento, lineaActiva);
+            imprimirInterfaz(documento, lineaActiva);
+            String comando = solicitarComando();
+
+            if (comando.equals("S")) {
+                ejecutar = false;
+            } else if (comando.equals("D")) {
+                documento = deshacerAccion(backup);
+            } else {
+                backup = documento.clone();
+                if (comando.equals("L")) {
+                    lineaActiva = seleccionarLinea();
+                } else if (comando.equals("E")) {
+                    editarLinea(documento, lineaActiva);
+                } else if (comando.equals("I")) {
+                    intercambiarLineas(documento);
+                } else if (comando.equals("B")) {
+                    borrarLinea(documento, lineaActiva);
+                }
             }
+        } while (ejecutar);
 
-        } while (opcion != COMANDO_SALIR);
-
-        System.out.println("Cierre de pyEdlin exitoso.");
     }
 
-    static String[] crearDocumento() {
+    static String[] obtenerDocumentoInicial() {
         return new String[] {
-            "", "", "", "", "", "", "", "", "", ""
+            "Bienvenidos al editor EDLIN",
+            "Utilice el menu inferior para editar el texto",
+            "------",
+            "[L] permite definir la linea activa",
+            "[E] permite editar la linea activa",
+            "[I] permite intercambiar dos lineas",
+            "[B] borra el contenido de la linea activa",
+            "[D] deshace la última acción realizada",
+            "[S] sale del programa",
+            " "
         };
     }
 
-    static int solicitarOpcion() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n[1] Seleccionar Línea | [2] Editar | [3] Eliminar | [0] Salir");
-        System.out.print("Comando > ");
-        return scanner.nextInt();
+    static String solicitarComando() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [D]eshacer | [S]alir\n> ");
+        return sc.nextLine().toUpperCase();
     }
 
-    static int seleccionarLinea(int totalLineas) {
-        Scanner scanner = new Scanner(System.in);
+    static int seleccionarLinea() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Número de línea (0-9): ");
-        int seleccion = scanner.nextInt();
-        return (seleccion >= 0 && seleccion < totalLineas) ? seleccion : 0;
+        return sc.nextInt();
     }
 
-    static void editarLinea(String[] documento, int indice) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nuevo contenido: ");
-        String contenido = scanner.nextLine();
-        documento[indice] = contenido;
+    static void editarLinea(String[] doc, int indice) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nuevo texto para línea " + indice + ": ");
+        doc[indice] = sc.nextLine();
     }
 
-    static void eliminarContenido(String[] documento, int indice) {
-        documento[indice] = "";
+    static void borrarLinea(String[] doc, int indice) {
+        doc[indice] = " ";
     }
 
-    static void imprimirEditor(String[] documento, int activa) {
-        limpiarPantalla();
-        System.out.println("pyEdlin - Editor de texto");
-        System.out.println("-------------------------");
-        for (int i = 0; i < documento.length; i++) {
-            String marcador = (i == activa) ? " > " : "   ";
-            System.out.println(i + marcador + documento[i]);
+    static void intercambiarLineas(String[] doc) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Línea origen: ");
+        int origen = sc.nextInt();
+        System.out.print("Línea destino: ");
+        int destino = sc.nextInt();
+
+        String temporal = doc[origen];
+        doc[origen] = doc[destino];
+        doc[destino] = temporal;
+    }
+
+    static String[] deshacerAccion(String[] backup) {
+        return backup.clone();
+    }
+
+    static void imprimirInterfaz(String[] doc, int activa) {
+        cleanScreen();
+        System.out.println("--------------------------------------------------");
+        for (int i = 0; i < doc.length; i++) {
+            String marcador = (i == activa) ? "*|" : " |";
+            System.out.println(i + ":" + marcador + " " + doc[i]);
         }
-        System.out.println("-------------------------");
-        System.out.println("Línea activa: " + activa);
+        System.out.println("--------------------------------------------------");
     }
 
-    static void limpiarPantalla() {
+    static void cleanScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
