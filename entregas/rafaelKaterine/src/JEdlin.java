de4a249d4c1cbafa3bc0f19b5f6f0b236b6f4cf6
katerine <katerine.rafael@alumnos.uneatlantico.es>
2026-02-10 11:43:20 +0100

    Creacion de clase principal, declaracion y inicializacion de variables

--- entregas/rafaelKaterine/src/JEdlin.java
+++ entregas/rafaelKaterine/src/JEdlin.java
@@ -1,7 +1,20 @@
 
 
-public class JEdlin {
+public class main {
+    Scanner sc = new Scanner(System.in);
+    String[] linea = new String[10];
+    int punteroLinea = 1;
 
+    linea[0] = "Bienvenidos al editor de texto JEdlin";
+    linea[1] = "Utilice el menu inferior para editar el texto";
+    linea[2] = " ------------------------------------";
+    linea[3] = "[L] permite definir la linea activa";
+    linea[4] = "[E] permite editar la linea activa";
+    linea[5] = "[I] permite intercambiar dos lineas";
+    linea[6] = "[B] borra el contenido de la linea activa";
+    linea[7] = "[D] deshace la última acción realizada";
+    linea[8] = "[S] sale del programa";
+    linea[9] = " ------------------------------------";
 }
 
 
