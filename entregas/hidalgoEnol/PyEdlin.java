9d0b677785f214ee1f88da5f880b0b6cc237b8f2
eenol <enol.hidalgo@alumnos.uneatlantico.es>
2026-02-10 14:05:30 +0100

    feat: estructura de almacenamiento de lineas

--- entregas/hidalgoEnol/PyEdlin.java
+++ entregas/hidalgoEnol/PyEdlin.java
@@ -1,5 +1,12 @@
 public class PyEdlin {
     public static void main(String[] args) {
+
+        String[] lineas = new String[10];
+
+        for (int i = 0; i < lineas.length; i++) {
+            lineas[i] = "";
+        }
+
         System.out.println("Bienvenido a pyEdlin");
     }
 }
