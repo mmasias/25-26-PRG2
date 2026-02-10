a6c567323e888326a7266ef6f8bfce2d7a425631
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-02-10 13:12:46 +0100

    Segundo paso:
    Inicialización del editor y datos iniciales

--- entregas/Castañeda.Guillermo/Reto-002/Edlin.java
+++ entregas/Castañeda.Guillermo/Reto-002/Edlin.java
@@ -6,4 +6,22 @@ public class Edlin {
     static String[] archivo = new String[MAX_LINEAS];
     static int lineaActiva = 0;
     static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < MAX_LINEAS; i++) {
+            archivo[i] = "";
+        }
+
+        archivo[0] = "Sea bienvenido al editor EDLIN";
+        archivo[1] = "Utilice el menu inferior para editar las líneas de texto";
+        archivo[2] = "---------------------------------------------------------";
+        archivo[3] = "[L] Definir la linea activa";
+        archivo[4] = "[E] Editar la linea activa";
+        archivo[5] = "[I] Intercambiar dos lineas";
+        archivo[6] = "[B] Borrar el contenido de la linea activa";
+        archivo[7] = "[S] Salir del programa";
+
+        boolean salir = false;
+    }
 }
