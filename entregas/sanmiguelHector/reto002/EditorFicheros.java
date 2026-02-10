0bead22d0f2a20a640b11d699c42d13ad72507f7
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-02-10 14:11:40 +0100

    Creado el método "MostrarFichero"

--- entregas/sanmiguelHector/reto002/EditorFicheros.java
+++ entregas/sanmiguelHector/reto002/EditorFicheros.java
@@ -1,34 +1,51 @@
 package entregas.sanmiguelHector.reto002;
 
 import java.util.Scanner;
+
 public class EditorFicheros {
+
+    static String[] fichero;
+    static int lineaActiva = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        MostrarMenu();
+        String opcionElegidaPorUsuario;
+
         CrearFichero();
 
-        String opcionElegidaPorUsuario = scanner.next();
+        do {
+            MostrarMenu();
+            MostrarFichero();
 
-        switch (opcionElegidaPorUsuario){
-            case "L": 
-                DefinirLineaActiva();
-        }
+            opcionElegidaPorUsuario = scanner.next();
 
+            switch (opcionElegidaPorUsuario) {
+                case "S":
+                    System.out.println("Saliendo del editor...");
+                    break;
+                case "L":
+                    int nuevaLinea = DefinirLineaActiva();
+                    if (nuevaLinea != -1) {
+                        lineaActiva = nuevaLinea;
+                    }
+                    break;
+            }
+
+        } while (!opcionElegidaPorUsuario.equals("S"));
     }
 
     static int DefinirLineaActiva() {
-        System.out.print("Seleccione línea (1-10): ");
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Seleccione línea (1-10): ");
         int linea = scanner.nextInt();
 
         if (linea < 1 || linea > 10) {
             System.out.println("Línea no válida.");
-            return - 1;
-        } else {
-            int lineaActiva = linea - 1;
-            System.out.println("Línea activa establecida en: " + linea);
-            return lineaActiva;
+            return -1;
         }
+
+        System.out.println("Línea activa establecida en: " + linea);
+        return linea - 1;
     }
 
     static void MostrarMenu() {
@@ -45,13 +62,21 @@ public class EditorFicheros {
         System.out.println("9: | ");
         System.out.println("--------------------------------------------------");
         System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir");
+        System.out.println();
     }
 
-    static String[] fichero;
     static void CrearFichero() {
         fichero = new String[10];
         for (int i = 0; i < fichero.length; i++) {
             fichero[i] = "";
         }
-    }    
+    }
+
+    static void MostrarFichero() {
+        System.out.println("------------------Fichero Actual------------------");
+        for (int i = 0; i < fichero.length; i++) {
+            String marca = (i == lineaActiva) ? "*" : " ";
+            System.out.println(i + marca + ": " + fichero[i]);
+        }
+    }
 }
