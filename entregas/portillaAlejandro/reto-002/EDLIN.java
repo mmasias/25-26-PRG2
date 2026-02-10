2a484ca1916d3683380cc6099f0b661565b8fdf3
alejandroportilla-create <alejandro.portilla@alumnos.uneatlantico.es>
2026-02-10 13:42:06 +0100

    Se inicializa el contenido del editor con un texto de guía y se implementa el bucle principal que muestra el contenido y solicita comandos, permitiendo por ahora solo salir del programa. Incluye los métodos de inicialización y visualización, así como las variables necesarias y la constante MAX_LINEAS.

--- entregas/portillaAlejandro/reto-002/EDLIN.java
+++ entregas/portillaAlejandro/reto-002/EDLIN.java
@@ -7,6 +7,48 @@ import java.util.Scanner;
 
     public static void main(String[] args) {
         System.out.println("Editor iniciado");
-        
+                inicializarTexto();
+        boolean salir = false;
+
+        while (!salir) {
+            mostrarFichero();
+            System.out.print("Comando: ");
+            String comando = scanner.nextLine().toUpperCase();
+
+            switch (comando) {
+                case "L":
+                    cambiarLineaActiva();
+                    break;
+                case "E":
+                    editarLinea();
+                    break;
+                case "B":
+                    borrarLinea();
+                    break;
+                case "I":
+                    intercambiarLineas();
+                    break;
+                case "S":
+                    salir = true;
+                    System.out.println("Saliendo del editor...");
+                    break;
+            }
+        }
     }
+
+    private static void inicializarTexto() {
+        for (int i = 0; i < MAX_LINEAS; i++) {
+            lineas[i] = "";
+        }
+
+        lineas[0] = "Bienvenidos al editor EDLIN";
+        lineas[1] = "Utilice el menu inferior para editar el texto";
+        lineas[2] = "------";
+        lineas[3] = "[L] permite definir la linea activa";
+        lineas[4] = "[E] permite editar la linea activa";
+        lineas[5] = "[I] permite intercambiar dos lineas";
+        lineas[6] = "[B] borra el contenido de la linea activa";
+        lineas[7] = "[S] sale del programa";
+    }
+
 
