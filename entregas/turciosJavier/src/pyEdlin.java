25d60238e1957216a7968c289bbf5d2b3b6009e9
javier.turcios <ulricjavier@gmail.com>
2026-02-10 12:23:37 +0000

    feat: agregar estructura básica del array

--- entregas/turciosJavier/src/pyEdlin.java
+++ entregas/turciosJavier/src/pyEdlin.java
@@ -1,1 +1,16 @@
 import java.util.Scanner;
+
+public class pyEdlin {
+	
+
+	public static void main(String[] args) {
+		String[] lineas = new String[10];
+		int lineaActiva = 0;
+
+		Scanner scanner = new Scanner(System.in);
+		boolean running = true;
+
+        
+		
+}
+
