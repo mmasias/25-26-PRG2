6bfc5b7282f93fd839ba7dbdeb2b2171cf058cde
izanherranz07 <izan.herranz@alumnos.uneatlantico.es>
2026-02-10 13:38:37 +0100

    Gestión de línea activa

--- entregas/herranzIzan/Reto-002/pyEdlin.java
+++ entregas/herranzIzan/Reto-002/pyEdlin.java
@@ -4,6 +4,7 @@ public class Edlin {
 
     private static final int MAX_LINES = 10;
     private static String[] file = new String[MAX_LINES];
+    private static int activeLine = 0;
 
     public static void main(String[] args) {
         // Inicializar líneas
@@ -12,12 +13,14 @@ public class Edlin {
         }
 
         printFile();
+        System.out.println("Línea activa: " + (activeLine + 1));
     }
     
     private static void printFile() {
         System.out.println("----- FICHERO -----");
         for (int i = 0; i < MAX_LINES; i++) {
-            System.out.println((i + 1) + ": " + file[i]);
+            String marker = (i == activeLine) ? ">" : " ";
+            System.out.println(marker + " " + (i + 1) + ": " + file[i]);
         }
         System.out.println("-------------------");
     }
