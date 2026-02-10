db62b45dcc767cecffff4759ebba39125ca86bf2
Samuel Alejandro Herrera Castillo <samuel.herrera@unealum.local>
2026-02-10 13:32:45 +0100

    Estructura inicial: declaración de arreglos para texto e historial, y variable de línea activa.

--- entregas/herreraSamuel/src/reto-002/PyEdlin.java
+++ entregas/herreraSamuel/src/reto-002/PyEdlin.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
+public class PyEdlin {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        String[] lineasDeTexto = new String[10];
+        String[] historial = new String[10];
+        int LineaActiva = 0;
+
+    }
+
+}
 
 
