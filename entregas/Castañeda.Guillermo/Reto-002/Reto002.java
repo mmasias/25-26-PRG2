5523e808cddd82687a6dc53b7b1c9db6093c0c6e
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-02-10 13:07:01 +0100

    Primer paso:
    Importación de variables y creación de clase

---
+++ entregas/Castañeda.Guillermo/Reto-002/Reto002.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Reto002 {
+
+    static final int MAX_LINEAS = 10;
+    static String[] archivo = new String[MAX_LINEAS];
+    static int lineaActiva = 0;
+    static Scanner sc = new Scanner(System.in);
+}
