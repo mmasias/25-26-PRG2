845a21f74b57dc6f96412ec2567d3cbfe314d298
yagolopez918-cloud <yagolopez918@gmail.com>
2026-02-10 13:43:47 +0100

    Añadir bucle principal y comando salir

--- entregas/lopezyago/pyEdlin.java
+++ entregas/lopezyago/pyEdlin.java
@@ -1,15 +1,25 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class pyEdlin {
+public class PyEdlin {
 
     static final int MAX_LINEAS = 10;
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         String[] lineas = new String[MAX_LINEAS];
         Arrays.fill(lineas, "");
 
-        mostrar(lineas);
+        boolean salir = false;
+
+        while (!salir) {
+            mostrar(lineas);
+            System.out.println("[S] Salir");
+
+            String cmd = sc.nextLine().toUpperCase();
+            if (cmd.equals("S")) salir = true;
+        }
     }
 
     static void mostrar(String[] lineas) {
@@ -19,3 +29,6 @@ public class pyEdlin {
         }
     }
 }
+
+
+
