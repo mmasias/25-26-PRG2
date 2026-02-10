af2dd852055903778ee75c5d4f8a81b87684d14d
marcosacevedo-gif <marcos.acevedo@uneatlantico.es>
2026-02-10 13:34:42 +0100

    Se añaden los elementos necesarios para gestionar la información del editor: una estructura para almacenar las líneas de texto, un indicador para la línea seleccionada y el sistema de entrada por teclado. También se define el número máximo de líneas con el que trabajará el editor.

--- entregas/AcevedoMarcos/reto-002/EDLIN.java
+++ entregas/AcevedoMarcos/reto-002/EDLIN.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 
+
 public class EDLIN {
+    
+private static final int MAX_LINEAS = 10;
+private static String[] lineas = new String[MAX_LINEAS];
+private static int lineaActiva = 0;
+private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Editor iniciado");
+
     }
 }
