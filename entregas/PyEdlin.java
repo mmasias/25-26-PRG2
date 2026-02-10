839076be7543b55201569144a5e14ba57370441f
rodrigorgo1909-web <rodrigorgo1909@gmail.com>
2026-02-10 13:04:20 +0100

    representamos el archivo

--- entregas/PyEdlin.java
+++ entregas/PyEdlin.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class PyEdlin {
 
+    static String[] lineas = new String[10];
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("PyEdlin iniciado");
+
+        for(int i = 0; i < 10; i++){
+            lineas[i] = "";
+        }
+
+        System.out.println("Archivo inicializado");
+
         sc.close();
     }
 }
