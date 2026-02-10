52b9290868833965684e658bea1765e711871bf8
lucasmorales-crypto <lucas.morales@alumnos.uneatlantico.es>
2026-02-10 13:46:45 +0100

    Se desarrolla la funcionalidad encargada de mostrar el estado actual del editor. Se imprimen todas las líneas numeradas, se resalta la línea seleccionada y se muestra un resumen de los comandos disponibles.

--- entregas/MoralesLucas/reto-002/EDLIN.java
+++ entregas/MoralesLucas/reto-002/EDLIN.java
@@ -10,7 +10,7 @@ public class EDLIN {
 
     public static void main(String[] args) {
         System.out.println("Editor iniciado");
-          inicializarTexto();
+        inicializarTexto();
         boolean salir = false;
 
         while (!salir) {
@@ -53,5 +53,18 @@ public class EDLIN {
         lineas[6] = "[B] borra el contenido de la linea activa";
         lineas[7] = "[S] sale del programa";
     }
+    private static void mostrarFichero() {
+        System.out.println("--------------------------------------------------");
+        for (int i = 0; i < MAX_LINEAS; i++) {
+            String marca;
+            if (i == lineaActiva) {
+                marca = "*";
+            } else {
+                marca = " ";
+            }
+            System.out.println(i + ":" + marca + "| " + lineas[i]);
+        }
+        System.out.println("--------------------------------------------------");
+        System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir");
     }
 }
