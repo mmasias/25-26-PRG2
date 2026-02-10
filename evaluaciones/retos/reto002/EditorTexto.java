8a0aeb65f5dc6c34a6d5f97e237e019cf21ab618
kevin <kevin.ponce@alumnos.uneatlantico.es>
2026-02-10 13:19:25 +0100

    correcion de declaracion de variables y defincion de fichero

--- evaluaciones/retos/reto002/EditorTexto.java
+++ evaluaciones/retos/reto002/EditorTexto.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class EditorTexto {
 
-    static String[] fichero;
+    static final int TAM_FICHERO = 10;
+    static String[] fichero = new String[TAM_FICHERO];
     static int lineaActiva = 0;
     static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
-        EditorTexto editor = new EditorTexto();
-        fichero = new String[10];
-    
+
         boolean salir = false;
-    
+
         do {
-            editor.imprimirFichero();
-            editor.mostrarMenu();
+            imprimirFichero();
+            mostrarMenu();
             System.out.print("Elige una opcion: ");
-    
+
             String opcion = teclado.next().toUpperCase();
-    
+
             switch (opcion) {
-                case "L" -> editor.definirLineaActiva();
+                case "L" -> definirLineaActiva();
                 case "E" -> editarLinea();
                 case "I" -> intercambiarLinea();
                 case "B" -> elimiarLinea();
@@ -31,12 +31,12 @@ public class EditorTexto {
                 }
                 default -> System.out.println("Opcion no valida");
             }
-    
+
         } while (!salir);
     }
-    
 
-    public void imprimirFichero() {
+
+    public static void imprimirFichero() {
         System.out.println("--------------------------------------------------");
         for (int i = 0; i < fichero.length; i++) {
             String contenido = (fichero[i] == null) ? "" : fichero[i];
@@ -49,38 +49,37 @@ public class EditorTexto {
         System.out.println("--------------------------------------------------");
     }
 
-    public void mostrarMenu() {
-        System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [D]eshacer | [S]alir");
+    public static void mostrarMenu() {
+        System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir");
     }
 
-    public static void editarLinea() {
-        System.out.print("Introduce el contenido de la nueva linea: ");
-        
-        teclado.nextLine();
-        String nuevoContenido = teclado.nextLine();
-    
-        fichero[lineaActiva] = nuevoContenido;
-    }
-    
-    public void definirLineaActiva() {
+    public static void definirLineaActiva() {
         System.out.print("Introduce la linea activa (1-10): ");
         int linea = teclado.nextInt();
-    
-        if (linea >= 1 && linea <= 10) {
+
+        if (linea >= 1 && linea <= TAM_FICHERO) {
             lineaActiva = linea - 1;
         } else {
             System.out.println("Error: la linea debe estar entre 1 y 10");
         }
     }
-    
+
+    public static void editarLinea() {
+        System.out.print("Introduce el contenido de la nueva linea: ");
+
+        teclado.nextLine();
+        String nuevoContenido = teclado.nextLine();
+
+        fichero[lineaActiva] = nuevoContenido;
+    }
 
     public static void intercambiarLinea() {
         System.out.print("Introduce la linea con la que quieres intercambiar (1-10): ");
         int otraLinea = teclado.nextInt();
-    
-        if (otraLinea >= 1 && otraLinea <= 10) {
+
+        if (otraLinea >= 1 && otraLinea <= TAM_FICHERO) {
             int indiceOtra = otraLinea - 1;
-    
+
             String aux = fichero[lineaActiva];
             fichero[lineaActiva] = fichero[indiceOtra];
             fichero[indiceOtra] = aux;
@@ -88,12 +87,8 @@ public class EditorTexto {
             System.out.println("Error: la linea debe estar entre 1 y 10");
         }
     }
-    
 
     public static void elimiarLinea() {
         fichero[lineaActiva] = null;
     }
-    
-
-    
 }
