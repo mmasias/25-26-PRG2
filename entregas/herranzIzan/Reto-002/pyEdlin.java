d9faab6641b438b0a307e16405593932e266d24b
izanherranz07 <izan.herranz@alumnos.uneatlantico.es>
2026-02-10 13:33:23 +0100

    Estructura base del editor y buffer de 10 líneas

--- entregas/herranzIzan/Reto-002/pyEdlin.java
+++ entregas/herranzIzan/Reto-002/pyEdlin.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class Edlin {
+
+    private static final int MAX_LINES = 10;
+    private static String[] file = new String[MAX_LINES];
+
+    public static void main(String[] args) {
+        // Inicializar líneas
+        for (int i = 0; i < MAX_LINES; i++) {
+            file[i] = "";
+        }
+
+        System.out.println("EDLIN iniciado");
+    }
+}
 
