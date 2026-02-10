3cd0588685749ec8352823558ba49b54e7bff041
Asier Garrastazu <asier.gc20072gmail.com>
2026-02-10 13:49:39 +0100

    muchos cambios explicados a continuación Organizacion en metodos estaticos Cambios de las variables por el nombre de los metodos estaticos Se han añadido todas las funciones del programa

--- entregas/garrastazuAsier/src/Edlin.java
+++ entregas/garrastazuAsier/src/Edlin.java
@@ -1,47 +1,89 @@
 import java.util.Scanner;
 
-public class Edlin {
+class Edlin {
+
+    static String[] archivo = new String[10];
+    static int lineaActiva = 0;
+    static Scanner sc = new Scanner(System.in);
+
+    static void ejecutar() {
+        for (int i = 0; i < 10; i++) archivo[i] = "";
+        while (true) {
+            mostrarArchivo();
+            mostrarMenu();
+
+            System.out.print("\nEscribe el comando: ");
+            String comando = sc.nextLine();
+
+            if (comando.equalsIgnoreCase("seleccionarLinea")) {
+                seleccionarLinea();
+            } else if (comando.equalsIgnoreCase("editarLinea")) {
+                editarLinea();
+            } else if (comando.equalsIgnoreCase("borrarLinea")) {
+                borrarLinea();
+            } else if (comando.equalsIgnoreCase("intercambiarLineas")) {
+                intercambiarLineas();
+            } else if (comando.equalsIgnoreCase("salir")) {
+                System.out.println("Saliendo...");
+                break;
+            } else {
+                System.out.println("Comando inválido. Intenta de nuevo.");
+            }
+        }
+    }
+
+    static void mostrarArchivo() {
+        System.out.println("\nArchivo:");
+        for (int i = 0; i < 10; i++) {
+            if (i == lineaActiva)
+                System.out.println("> " + (i + 1) + ": " + archivo[i]);
+            else
+                System.out.println("  " + (i + 1) + ": " + archivo[i]);
+        }
+    }
+
+    static void mostrarMenu() {
+        System.out.println("\nComandos disponibles:");
+        System.out.println("- seleccionarLinea");
+        System.out.println("- editarLinea");
+        System.out.println("- borrarLinea");
+        System.out.println("- intercambiarLineas");
+        System.out.println("- salir");
+    }
+
+    static void seleccionarLinea() {
+        System.out.print("Número de línea a activar (1-10): ");
+        int linea = sc.nextInt() - 1;
+        sc.nextLine();
+        if (linea >= 0 && linea < 10) lineaActiva = linea;
+    }
+
+    static void editarLinea() {
+        System.out.print("Texto para la línea activa: ");
+        archivo[lineaActiva] = sc.nextLine();
+    }
+
+    static void borrarLinea() {
+        System.out.print("Número de línea a borrar (1-10): ");
+        int linea = sc.nextInt() - 1;
+        sc.nextLine();
+        if (linea >= 0 && linea < 10) archivo[linea] = "";
+    }
+
+    static void intercambiarLineas() {
+        System.out.print("Número de la primera línea a intercambiar (1-10): ");
+        int linea1 = sc.nextInt() - 1;
+        System.out.print("Número de la segunda línea a intercambiar (1-10): ");
+        int linea2 = sc.nextInt() - 1;
+        sc.nextLine();
+        if (linea1 >= 0 && linea1 < 10 && linea2 >= 0 && linea2 < 10) {
+            String temp = archivo[linea1];
+            archivo[linea1] = archivo[linea2];
+            archivo[linea2] = temp;
+        }
+    }
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        String[] lineas = new String[10];
-        int activa = 0;
-
-        for (int i = 0; i < 10; i++) lineas[i] = "";
-
-        while (true) {
-
-            System.out.println("\nArchivo:");
-            for (int i = 0; i < 10; i++) {
-                if (i == activa)
-                    System.out.println("> " + (i + 1) + ": " + lineas[i]);
-                else
-                    System.out.println("  " + (i + 1) + ": " + lineas[i]);
-            }
-
-            System.out.println("\n1 Seleccionar");
-            System.out.println("2 Editar");
-            System.out.println("3 Borrar");
-            System.out.println("4 Intercambiar");
-            System.out.println("0 Salir");
-
-            int op = sc.nextInt();
-            sc.nextLine();
-
-            if (op == 1) activa = sc.nextInt() - 1;
-            if (op == 2) lineas[activa] = sc.nextLine();
-            if (op == 3) lineas[sc.nextInt() - 1] = "";
-
-            if (op == 4) {
-                int a = sc.nextInt() - 1;
-                int b = sc.nextInt() - 1;
-                String temp = lineas[a];
-                lineas[a] = lineas[b];
-                lineas[b] = temp;
-            }
-
-            if (op == 0) break;
-        }
+        ejecutar();
     }
 }
