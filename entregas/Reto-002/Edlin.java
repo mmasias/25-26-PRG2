763dd4351b9e0cc3a9073d95c91b10914b99f3ce
luis.hoyos <felipehoyos2008@gmail.com>
2026-02-10 13:09:56 +0100

    agrego contenido de lineas

--- entregas/Reto-002/Edlin.java
+++ entregas/Reto-002/Edlin.java
@@ -7,9 +7,19 @@ public class Edlin {
         String[] lineas = new String[10];
         int lineaActiva = 1;
         
+        lineas[0] = "Bienvenidos al editor EDLIN";
+        lineas[1] = "Utilice el menu inferior para editar el texto";
+        lineas[2] = "------";
+        lineas[3] = "[L] permite definir la linea activa";
+        lineas[4] = "[E] permite editar la linea activa";
+        lineas[5] = "[I] permite intercambiar dos lineas";
+        lineas[6] = "[B] borra el contenido de la linea activa";
+        lineas[7] = "[D] deshace la ultima accion realizada";
+        lineas[8] = "[S] sale del programa";
+        lineas[9] = "";
+        
         System.out.println("Editor EDLIN - Iniciando...");
-        System.out.println("Tengo " + lineas.length + " lineas disponibles.");
-        System.out.println("Linea activa: " + lineaActiva);
+        System.out.println("Contenido cargado: " + lineas[0]);
         
         sc.close();
     }
