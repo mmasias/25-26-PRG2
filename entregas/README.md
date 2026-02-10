94fa1f67782c65deb5e80b1595b02a30e241063f
javierpaguaga-bit <javier.paguaga@alumnos.uneatlantico.es>
2026-02-10 13:57:53 +0100

    refactor: Se ha cambiado como se mirar las líneas. Ahora en ves de ser por un comando, sera algo que constantemente se mira

--- entregas/README.md
+++ entregas/README.md
@@ -16,8 +16,10 @@ Crea la primera versión de edlin, que pueda gestionar un fichero de 10 líneas
 
 2. En base a lo definido arriba, plantee como implementaría la opción de deshacer (CTRL+Z).
 
-# Pasos de la solución
+# Pasos tomados para la solución
 
 1. Creación de la clase pyEdlin y el boceto de que hacer en el README.
 
 2. Crear la opciones de mirar, crear, modificar lineas o cerrar el programma.
+
+3. Quitar el la opcion de commando "mirar" haciendo que sea algo ya hecho constantemente en el programma, teniendo la idea de que la introduccion del programma ya este siendo las primeras lineas, las cuales deberan poderse modificar y hacerunos arreglos gramaticos.
--- entregas/paguagaJavier/src/pyEdlin.java
+++ entregas/paguagaJavier/src/pyEdlin.java
@@ -4,16 +4,42 @@ public class pyEdlin {
     public static void main(String[] args) {
 
     Boolean edlinEncendido = true;
+    String[] lineas = new String[10];
+    lineas[0] = "Bienvenido a Edlin";
+    lineas[1] = "Un editor de texto";
+    lineas[2] = "donde puedes escribir hasta 10 líneas de texto.";
+    lineas[3] = "";
+    lineas[4] = "";
+    lineas[5] = "";
+    lineas[6] = "";
+    lineas[7] = "";
+    lineas[8] = "";
+    lineas[9] = "";
     Scanner comando = new Scanner(System.in);
+
     do {
-        System.out.print("Elegir un comando: [1]Mirar lineas, [2]Agredar linea, [3]Moficar linea, [4]Salir: ");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("0|" + (lineas[0]));
+        System.out.println("1|" + (lineas[1]));
+        System.out.println("2|" + (lineas[2]));
+        System.out.println("3|" + (lineas[3]));
+        System.out.println("4|" + (lineas[4]));
+        System.out.println("5|" + (lineas[5]));
+        System.out.println("6|" + (lineas[6]));
+        System.out.println("7|" + (lineas[7]));
+        System.out.println("8|" + (lineas[8]));
+        System.out.println("9|" + (lineas[9]));
+        System.out.println("------------------------------------------------------------------");
+
+        System.out.print("Elegir un comando: [1]Añadir linea, [2]Quitar linea, [3]Moficar linea, [4]Salir: ");
         String comandoElegido = comando.nextLine();
         switch (comandoElegido) {
             case "1" -> {
-                System.out.println("Mostrando líneas...");
+                System.out.println("Añadiendo línea...");
             }
+
             case "2" -> {
-                System.out.println("Agregando línea...");
+                System.out.println("Borrando línea...");
             }
 
             case "3" -> {
