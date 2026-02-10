b3c0150b0caa8095f9b432cca0428eacbfaa2b16
luis.hoyos <felipehoyos2008@gmail.com>
2026-02-10 13:07:48 +0100

    agregar estructuras de datos basicas

--- entregas/Reto-002/Edlin.java
+++ entregas/Reto-002/Edlin.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Edlin {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String[] lineas = new String[10];
+        int lineaActiva = 1;
+        
         System.out.println("Editor EDLIN - Iniciando...");
+        System.out.println("Tengo " + lineas.length + " lineas disponibles.");
+        System.out.println("Linea activa: " + lineaActiva);
+        
+        sc.close();
     }
 }
