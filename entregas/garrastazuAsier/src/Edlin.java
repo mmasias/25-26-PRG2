b141af906b43fb7606d9b7d15e97fbe2590c27b6
Asier Garrastazu <asier.gc20072gmail.com>
2026-02-10 13:30:45 +0100

    Creo codigo (con mejoras posibles)

--- entregas/garrastazuAsier/src/Edlin.java
+++ entregas/garrastazuAsier/src/Edlin.java
@@ -1,1 +1,30 @@
 
+import java.util.Scanner;
+
+class Edlin {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String[] lineas = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            lineas[i] = "";
+        }
+
+        while (true) {
+
+            System.out.println("\nArchivo:");
+            for (int i = 0; i < 10; i++) {
+                System.out.println((i + 1) + ": " + lineas[i]);
+            }
+
+            System.out.println("\nEscribe texto para la línea 1 (0 para salir):");
+            String texto = sc.nextLine();
+
+            if (texto.equals("0")) break;
+
+            lineas[0] = texto;
+        }
+    }
+}
