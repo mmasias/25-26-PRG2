8796beeec0506c9e3280900238bcac3f80f325a2
Danielmora <daniel.mora@alumnos.uneatlantico.es>
2026-02-10 13:38:11 +0100

    Finalizado el reto

--- entregas/moraDaniel/MiPropioEdlin.java
+++ entregas/moraDaniel/MiPropioEdlin.java
@@ -9,6 +9,36 @@ public class MiPropioEdlin {
         Scanner scanner = new Scanner(System.in);
         String[] lineas = crearArrayLineas();
         String[] copiaAnterior = crearArrayLineas();
+        int lineaActiva = 1;
+        boolean salir = false;
+        while (!salir) {
+            mostrarEditor(lineas, lineaActiva);
+            String comando = leerComando(scanner);
+            switch (comando) {
+                case "L":
+                    lineaActiva = cambiarLineaActiva(scanner, lineaActiva);
+                    break;
+                case "E":
+                    copiarEstado(lineas, copiaAnterior);
+                    editarLinea(scanner, lineas, lineaActiva);
+                    break;
+                case "B":
+                    copiarEstado(lineas, copiaAnterior);
+                    borrarLinea(lineas, lineaActiva);
+                    break;
+                case "I":
+                    copiarEstado(lineas, copiaAnterior);
+                    intercambiarLineas(scanner, lineas);
+                    break;
+                case "D":
+                    deshacer(lineas, copiaAnterior);
+                    break;
+                default:
+                    mostrarMensaje("Comando no reconocido ");
+
+            }
+        }
+        scanner.close();
 
     }
 
@@ -27,5 +57,76 @@ public class MiPropioEdlin {
         array[9] = "";
 
         return array;
+    }
+
+    static void mostrarEditor(String[] lineas, int lineaActiva) {
+        for (int i = 0; i < MAX_LINEAS; i++) {
+            if (i == lineaActiva) {
+                System.out.println(i + ":*| " + lineas[i]);
+            } else {
+                System.out.println(i + ": | " + lineas[i]);
+            }
+        }
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [D]eshacer | [S]alir");
+
+    }
+
+    static String leerComando(Scanner scanner) {
+        System.out.print("Seleccione comando: ");
+        return scanner.nextLine().toUpperCase();
+    }
+
+    static void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    static int cambiarLineaActiva(Scanner scanner, int lineaActual) {
+        System.out.print("Ingrese número de línea (0-9): ");
+        int nuevaLinea = Integer.parseInt(scanner.nextLine());
+
+        if (nuevaLinea >= 0 && nuevaLinea < MAX_LINEAS) {
+            return nuevaLinea;
+        } else {
+            mostrarMensaje("Número inválido.");
+            return lineaActual;
+        }
+    }
+
+    static void editarLinea(Scanner scanner, String[] lineas, int lineaActiva) {
+        System.out.print("Nuevo texto: ");
+        lineas[lineaActiva] = scanner.nextLine();
+    }
+
+    static void borrarLinea(String[] lineas, int lineaActiva) {
+        lineas[lineaActiva] = "";
+    }
+
+    static void intercambiarLineas(Scanner scanner, String[] lineas) {
+
+        System.out.print("Primera línea: ");
+        int a = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Segunda línea: ");
+        int b = Integer.parseInt(scanner.nextLine());
+
+        if (a >= 0 && a < MAX_LINEAS && b >= 0 && b < MAX_LINEAS) {
+            String temp = lineas[a];
+            lineas[a] = lineas[b];
+            lineas[b] = temp;
+        } else {
+            mostrarMensaje("Líneas inválidas.");
+        }
+    }
+
+    static void deshacer(String[] lineas, String[] copiaAnterior) {
+        copiarEstado(copiaAnterior, lineas);
+    }
+
+    static void copiarEstado(String[] origen, String[] destino) {
+        for (int i = 0; i < MAX_LINEAS; i++) {
+            destino[i] = origen[i];
+        }
     }
 }
