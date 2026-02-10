c13c839b46e927898f8f90daf0edef62ceeac693
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-02-10 13:02:38 +0100

    Declaro las variables y el scanner que van  a usar los metodos.

---
+++ entregas/AstorecaHugo/reto-002/edlin.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class edlin {
+   private static final int MAX_LINEAS = 100;
+   private static String[] lineas = new String [MAX_LINEAS];
+   private static int lineaActual = 0;
+   private static Scanner scanner = new Scanner(System.in);
+   
+    public static void main(String[] args) {
+        
+    }
+}
