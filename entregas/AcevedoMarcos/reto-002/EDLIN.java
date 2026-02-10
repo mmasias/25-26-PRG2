17e23469148e6a47561a6b422caeda6d851ec89b
marcosacevedo-gif <marcos.acevedo@uneatlantico.es>
2026-02-10 13:48:36 +0100

    En este paso se añaden las operaciones principales del editor. El usuario puede seleccionar una línea, modificar su contenido, borrar una línea o intercambiar el contenido de dos líneas distintas.

--- entregas/AcevedoMarcos/reto-002/EDLIN.java
+++ entregas/AcevedoMarcos/reto-002/EDLIN.java
@@ -66,6 +66,43 @@ private static void mostrarFichero() {
         }
         System.out.println("--------------------------------------------------");
         System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir");
+
     }
+     private static void cambiarLineaActiva() {
+        System.out.print("Número de línea (0-9): ");
+        int nueva = scanner.nextInt();
+        scanner.nextLine(); 
+
+        if (nueva >= 0 && nueva < MAX_LINEAS) {
+            lineaActiva = nueva;
+        }
+    }
+
+    private static void editarLinea() {
+        System.out.print("Nuevo contenido: ");
+        lineas[lineaActiva] = scanner.nextLine();
+    }
+
+    private static void borrarLinea() {
+        lineas[lineaActiva] = "";
+    }
+
+    private static void intercambiarLineas() {
+
+        System.out.print("Primera línea: ");
+        int l1 = scanner.nextInt();
+
+        System.out.print("Segunda línea: ");
+        int l2 = scanner.nextInt();
+        scanner.nextLine(); 
+
+        if (l1 >= 0 && l1 < MAX_LINEAS && l2 >= 0 && l2 < MAX_LINEAS) {
+            String temp = lineas[l1];
+            lineas[l1] = lineas[l2];
+            lineas[l2] = temp;
+        }
     }
 }
+
+    
+
