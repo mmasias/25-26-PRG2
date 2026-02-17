bdd5f516cd660884b7431d9de37cec83fc8f94d2
alejandroportilla-create <alejandro.portilla@alumnos.uneatlantico.es>
2026-02-17 13:46:23 +0100

    Creamos las matrices del tablero, inicializando los contadores a 0 y tambien llamamos a inicializarTablero y colocarMinas que seran creados posteriormente

--- entregas/portillaAlejandro/reto-003/Buscaminas.java
+++ entregas/portillaAlejandro/reto-003/Buscaminas.java
@@ -17,3 +17,12 @@ class Buscaminas {
     private boolean[][] tableroMinas;
     private int explosiones;
     private int casillasDescubiertas;
+    
+    public Buscaminas() {
+        tableroVisible = new String[FILAS][COLUMNAS];
+        tableroMinas = new boolean[FILAS][COLUMNAS];
+        explosiones = 0;
+        casillasDescubiertas = 0;
+        inicializarTablero();
+        colocarMinas();
+    }
