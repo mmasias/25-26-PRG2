541f0636226dbf9d2d2b52c9096ccc5394ae35cd
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-02-17 13:14:00 +0100

    Creación primeras variables
    Creación de las primeras variables y tableros

--- entregas/reto-003/ArroyoSanchezDaniel/BuscaMinas.java
+++ entregas/reto-003/ArroyoSanchezDaniel/BuscaMinas.java
@@ -1,3 +1,7 @@
 public class BuscaMinas {
+    final int MINAS_TOTALES = 5;
+    int posicionMinas[][] = new int[5][7];
+    int tableroVisible[][] = new int[5][7];
+    int minasDescubiertas = 0; 
 
 }
