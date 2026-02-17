379ce93e2ad0336ea82a5c46c4714a312f397681
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-02-17 13:20:52 +0100

    Creación metodo estático colocarMinas
    Creación del metodo estático colocarMinas, la adición de su respectivo código, y de este al main

--- entregas/reto-003/ArroyoSanchezDaniel/BuscaMinas.java
+++ entregas/reto-003/ArroyoSanchezDaniel/BuscaMinas.java
@@ -4,6 +4,20 @@ public class BuscaMinas {
         int posicionMinas[][] = new int[5][7];
         int tableroVisible[][] = new int[5][7];
         int minasDescubiertas = 0; 
+
+        colocarMinas(posicionMinas, MINAS_TOTALES);
     
+    }
+    static void colocarMinas(int posicionMinas[][], int MINAS_TOTALES ){
+        int minas = 0;
+        while (minas < MINAS_TOTALES) {
+            int px = (int) (Math.random() * 8);
+            int py = (int) (Math.random() * 8);
+            if (posicionMinas[py][px] == 0) {
+                minas++;
+                posicionMinas[py][px] = 1;
+            } else if (posicionMinas[py][px] == 1) {
+            }
+        }
     }
 }
