3863e6825726c0b245f1185653fa8a92977ed24f
lucasmorales-crypto <lucas.morales@alumnos.uneatlantico.es>
2026-02-17 13:46:31 +0100

    Creamos las matrices del tablero, inicializando los contadores a 0 y también llamamos a inicializar Tablero y colocarMinas que seran creados posteriormente.

--- entregas/MoralesLucas/reto-003/buscaminas.java
+++ entregas/MoralesLucas/reto-003/buscaminas.java
@@ -18,4 +18,12 @@ class Buscaminas {
     private int explosiones;
     private int casillasDescubiertas;
     
+    public Buscaminas() {
+        tableroVisible = new String[FILAS][COLUMNAS];
+        tableroMinas = new boolean[FILAS][COLUMNAS];
+        explosiones = 0;
+        casillasDescubiertas = 0;
+        inicializarTablero();
+        colocarMinas();
+    }
 }
