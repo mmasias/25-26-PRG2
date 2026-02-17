9cbe7f863f931d2ddcc268b5e9ec9de8e453aa94
Samuel Herrera <samyxelita@gmail.com>
2026-02-17 13:27:05 +0100

    feat : Establecer constantes para las bases del tablero y las reglas para la derrota

---
+++ entregas/herreraCastillo/src/reto-003/Buscaminas.java
@@ -0,0 +1,11 @@
+public class Buscaminas {
+
+    static final int TOTAL_FILAS = 5;
+    static final int TOTAL_COLUMNAS = 7;
+    static final int CANTIDAD_MINAS = 5;
+    static final int MAXIMO_EXPLOSIONES = 3;
+
+    public static void main(String[] args) {
+        
+    }
+}
