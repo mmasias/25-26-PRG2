247e2fb6cd120fd7083382248c81d114c24ca45a
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-02-17 13:04:24 +0100

    Añado minas aleatorias

--- entregas/Castañeda.Guillermo/Reto-003/Buscaminas.java
+++ entregas/Castañeda.Guillermo/Reto-003/Buscaminas.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Buscaminas {
@@ -6,12 +7,26 @@ public class Buscaminas {
 
         int filas = 5;
         int columnas = 7;
+        int minas = 5;
 
         String[][] tablero = new String[filas][columnas];
+        int[][] tableroMinas = new int[filas][columnas];
 
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 tablero[i][j] = "-";
+                tableroMinas[i][j] = 0;
+            }
+        }
+
+        Random random = new Random();
+        int minasColocadas = 0;
+        while (minasColocadas < minas) {
+            int f = random.nextInt(filas);
+            int c = random.nextInt(columnas);
+            if (tableroMinas[f][c] == 0) {
+                tableroMinas[f][c] = 1;
+                minasColocadas++;
             }
         }
 
