b528497f6d48ff249f361d1fda4abe9bbbb21b67
hoyos332 <hoyos332@gmail.com>
2026-02-17 13:10:03 +0100

    Estructura basica con constantes del juego

---
+++ entregas/hoyosfelipe/src/Reto-003/BuscaMinas.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BuscaMinas {
+    
+    static final int FILAS = 5;
+    static final int COLUMNAS = 7;
+    static final int TOTAL_MINAS = 5;
+    static final int MAX_EXPLOSIONES = 3;
+    
+    static char[][] tableroJuego;
+    
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al Buscaminas!");
+    }
+}
