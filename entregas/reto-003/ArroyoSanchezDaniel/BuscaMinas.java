cea8a6f21b93f1b208c953f5ad31a70a90f11bd4
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-02-17 13:35:25 +0100

    Creación metodo estático adivinarCasilla
    Creo la parte donde deberemos adivinar la casilla, con todo su código en el

--- entregas/reto-003/ArroyoSanchezDaniel/BuscaMinas.java
+++ entregas/reto-003/ArroyoSanchezDaniel/BuscaMinas.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class BuscaMinas {
     public static void main(String[] args) {
         final int MINAS_TOTALES = 5;
         int posicionMinas[][] = new int[5][7];
         int tableroVisible[][] = new int[5][7];
-        int minasDescubiertas = 0; 
+        int minasDescubiertas = 0;
 
         colocarMinas(posicionMinas, MINAS_TOTALES);
-    
+
     }
-    static void colocarMinas(int posicionMinas[][], int MINAS_TOTALES ){
+
+    static void colocarMinas(int posicionMinas[][], int MINAS_TOTALES) {
         int minas = 0;
         while (minas < MINAS_TOTALES) {
             int px = (int) (Math.random() * 8);
@@ -20,11 +23,48 @@ public class BuscaMinas {
             }
         }
     }
+
+    static int adivinarCasilla(int tableroVisible[][], int posicionMinas[][], int minasDescubiertas) {
+        Scanner scanner = new Scanner(System.in);
+        int posicionX;
+        int posicionY;
+        boolean fueraMapa;
+
+        do {
+            System.out.println("Ingrese la coordenada x, en número (1-7).");
+            posicionX = scanner.nextInt() - 1;
+            System.out.println("Ingrese la coordenada Y, en número (1-5).");
+            posicionY = scanner.nextInt() - 1;
+
+            fueraMapa = posicionX < 0 || posicionX > 6 || posicionY < 0 || posicionY > 4;
+
+            if (fueraMapa) {
+                System.out.println("Has marcado una posición fuera del Mapa, por favor elije de nuevo.");
+            }
+
+            if (tableroVisible[posicionY][posicionX] >= 0) {
+                System.out.println("Casilla descubierta, por favor elija otra");
+            } else if (tableroVisible[posicionY][posicionX] == -1) {
+                if (posicionMinas[posicionY][posicionX] == 1) {
+                    tableroVisible[posicionY][posicionX] = posicionMinas[posicionY][posicionX];
+                    minasDescubiertas++;
+
+                } else if (posicionMinas[posicionY][posicionX] == 0) {
+                    tableroVisible[posicionY][posicionX] = posicionMinas[posicionY][posicionX];
+                }
+            }
+
+        } while (tableroVisible[posicionY][posicionX] < 0 || fueraMapa);
+        return minasDescubiertas;
+
+    }
+
     static void imprimir(int tableroVisible[][]) {
         String posicion[] = { " . ", " * " };
-
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        System.out.println("    1  2  3  4  5  6  7");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i + " | ");
+            for (int j = 0; j < 7; j++) {
                 if (tableroVisible[i][j] == -1) {
                     System.out.print(" - ");
                 } else if (tableroVisible[i][j] >= 0) {
@@ -33,7 +73,6 @@ public class BuscaMinas {
             }
             System.out.println();
         }
+        System.out.println("================");
     }
 }
-
-
