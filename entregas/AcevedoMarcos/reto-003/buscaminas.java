3be73e49f7a4e081fb39c71276914c2334de57da
marcosacevedo-gif <marcos.acevedo@uneatlantico.es>
2026-02-17 13:43:48 +0100

    creamos la clase, el método y declaramos constantes y variables

---
+++ entregas/AcevedoMarcos/reto-003/buscaminas.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Buscaminas {
+
+    public static void main(String[] args) {
+        Buscaminas juego = new Buscaminas();
+        juego.jugar();
+    }
+
+    private static final int FILAS = 5;
+    private static final int COLUMNAS = 7;
+    private static final int MINAS = 5;
+    private static final int MAX_EXPLOSIONES = 3;
+
+    private String[][] tableroVisible;
+    private boolean[][] tableroMinas;
+    private int explosiones;
+    private int casillasDescubiertas;
