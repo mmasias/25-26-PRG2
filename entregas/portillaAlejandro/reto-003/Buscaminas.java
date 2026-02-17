86893648a794b8276a7045f186dbc226758194b9
alejandroportilla-create <alejandro.portilla@alumnos.uneatlantico.es>
2026-02-17 14:05:19 +0100

    Creamos la funcion que pone el resultado del juego

--- entregas/portillaAlejandro/reto-003/Buscaminas.java
+++ entregas/portillaAlejandro/reto-003/Buscaminas.java
@@ -109,5 +109,16 @@ class Buscaminas {
                 System.out.println("Libre!");
             }
         }
+        
+        mostrarTablero();
+
+        if (explosiones == MAX_EXPLOSIONES) {
+            System.out.println("Has perdido");
+        } else {
+            System.out.println("Has ganado");
+        }
+
+        scanner.close();
     }
+}
 
