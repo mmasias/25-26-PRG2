656e44838da5afb48cccddbc320a9f95ef75702c
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-02-17 13:20:20 +0100

    Creo contador minas restantes y tablero interactiv

--- entregas/Castañeda.Guillermo/Reto-003/Buscaminas.java
+++ entregas/Castañeda.Guillermo/Reto-003/Buscaminas.java
@@ -8,6 +8,7 @@ public class Buscaminas {
         int filas = 5;
         int columnas = 7;
         int minas = 5;
+        int maxExplosiones = 3;
 
         String[][] tablero = new String[filas][columnas];
         int[][] tableroMinas = new int[filas][columnas];
@@ -32,30 +33,57 @@ public class Buscaminas {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Ingrese X: ");
-        int x = sc.nextInt() - 1;
+        int explosiones = 0;
+        int descubiertas = 0;
+        int seguras = filas * columnas - minas;
 
-        System.out.print("Ingrese Y: ");
-        int y = sc.nextInt() - 1;
+        while (explosiones < maxExplosiones && descubiertas < seguras) {
 
-        if (tableroMinas[x][y] == 1) {
-            System.out.println("Mina!");
-            tablero[x][y] = "*";
-        } else {
-            System.out.println("Libre!");
-            tablero[x][y] = ".";
-        }
-
-        System.out.println("================");
-        System.out.println("  1 2 3 4 5 6 7");
-        for (int i = 0; i < filas; i++) {
-            System.out.print((i + 1) + " ");
-            for (int j = 0; j < columnas; j++) {
-                System.out.print(tablero[i][j] + " ");
+            System.out.println("================");
+            System.out.println("  1 2 3 4 5 6 7");
+            for (int i = 0; i < filas; i++) {
+                System.out.print((i + 1) + " ");
+                for (int j = 0; j < columnas; j++) {
+                    System.out.print(tablero[i][j] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
+            System.out.println("================");
+
+            System.out.print("Ingrese X: ");
+            int x = sc.nextInt() - 1;
+
+            System.out.print("Ingrese Y: ");
+            int y = sc.nextInt() - 1;
+
+            if (x >= 0 && x < filas && y >= 0 && y < columnas) {
+
+                if (tablero[x][y].equals("-")) {
+
+                    if (tableroMinas[x][y] == 1) {
+                        System.out.println("Mina!");
+                        tablero[x][y] = "*";
+                        explosiones++;
+                    } else {
+                        System.out.println("Libre!");
+                        tablero[x][y] = ".";
+                        descubiertas++;
+                    }
+
+                } else {
+                    System.out.println("Casilla ya usada");
+                }
+
+            } else {
+                System.out.println("Posicion no valida");
+            }
         }
-        System.out.println("================");
+
+        if (explosiones == maxExplosiones) {
+            System.out.println("Has perdido");
+        } else {
+            System.out.println("Has ganado");
+        }
 
         sc.close();
     }
