ec5cec8d35d5c41f065e830039efe66335f4bee3
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-02-17 13:31:36 +0100

    Creación tablero interactivo y registro mina rest.

--- entregas/Castañeda.Guillermo/Reto-003/Buscaminas.java
+++ entregas/Castañeda.Guillermo/Reto-003/Buscaminas.java
@@ -40,6 +40,7 @@ public class Buscaminas {
         while (explosiones < maxExplosiones && descubiertas < seguras) {
 
             System.out.println("================");
+            System.out.println("Minas restantes: " + (minas - explosiones));
             System.out.println("  1 2 3 4 5 6 7");
             for (int i = 0; i < filas; i++) {
                 System.out.print((i + 1) + " ");
