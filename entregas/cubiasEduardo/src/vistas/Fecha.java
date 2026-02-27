b6dc8911ae89a73a22f6f4939a1244698a7ca885
Eduardo Rene Cubias Moran <eduardo.cubias@unealum.local>
2026-02-27 11:41:28 +0100

    feat: add public methods

--- entregas/cubiasEduardo/src/vistas/Fecha.java
+++ entregas/cubiasEduardo/src/vistas/Fecha.java
@@ -5,4 +5,10 @@ class Fecha {
     public Fecha (){};
     public Fecha (Fecha fecha){};
 
+    public void mostrar(){};
+    public void editarFecha(int año, int mes, int dia){};
+    public Fecha realizarOperacionArtimeticaConUnaFecha(Fecha fecha, String operacion){ return new Fecha();};    
+    public boolean fechaEsMayor(Fecha fecha){return true;};
+    public int numeroSemana(){return 0;};
+
 }
