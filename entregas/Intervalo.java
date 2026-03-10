1f8e36c6295e078df61d9058569e8e6183f3aeae
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-03-10 11:08:37 +0100

    Final

--- entregas/Intervalo.java
+++ entregas/Intervalo.java
@@ -6,18 +6,31 @@ public class Intervalo {
     public Intervalo (Intervalo intervalo)
 
     public Intervalo clone()
+
     public double longitud()
+
     public double puntoMedio()
+
     public void desplazar (double desplazamiento)
+
     public Intervalo desplazado(double desplazamiento)
+
     public boolean incluye(double punto)
+
     public boolean incluye(Intervalo intervalo)
+
     public boolean equals(Intervalo intervalo)
+
     public Intervalo interseccion(Intervalo intervalo)
+
     public boolean intersecta(Intervalo intervalo)
+
     public void oponer()
+
     public void doblar()
+
     public void mostrar()
+    
     public void recoger()
 
 }
