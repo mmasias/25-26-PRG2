9c59170052bb8f096fe1ae134d5ef4cec195437b
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-03-10 10:58:47 +0100

    feat: agregar constructores a la clase Intervalo

---
+++ entregas/Intervalo.java
@@ -0,0 +1,8 @@
+public class Intervalo {
+
+    public Intervalo (double punoMedio, double logintud)
+    public Intervalo (double longitud)
+    public Intervalo ()
+    public Intervalo (Intervalo intervalo)
+    
+}
