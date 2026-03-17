49aabfa5bdad5b959a9411e9fe65cc2795b8d7cd
mateobayon8-hub <mateobayon8@gmail.com>
2026-03-17 13:52:24 +0100

    Añadir constructores básicos.

--- entregas/mateoBayon/src/Fraccion.java
+++ entregas/mateoBayon/src/Fraccion.java
@@ -2,4 +2,20 @@ public class Fraccion implements Cloneable, Comparable<Fraccion> {
 
     private int numerador;
     private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        if (denominador == 0) {
+            throw new IllegalArgumentException("El denominador no puede ser cero.");
+        }
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    public Fraccion(int numerador) {
+        this(numerador, 1);
+    }
+
+    public Fraccion() {
+        this(0, 1);
+    }
 }
