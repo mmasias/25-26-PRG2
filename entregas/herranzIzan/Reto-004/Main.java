73e67205a36a5d166db4746eed79fab1164c3d20
izanherranz07 <izan.herranz@alumnos.uneatlantico.es>
2026-03-17 14:02:35 +0100

    organizar salida de Main por bloques de prueba
    limpieza en el código

--- entregas/herranzIzan/Reto-004/Main.java
+++ entregas/herranzIzan/Reto-004/Main.java
@@ -6,26 +6,32 @@ public class Main {
         Fraccion f4 = new Fraccion(1, 2);
         Fraccion copia = f1.clone();
 
+        System.out.println("=== CREACIÓN DE FRACCIONES ===");
         System.out.println("f1 = " + f1);
         System.out.println("f2 = " + f2);
         System.out.println("f3 = " + f3);
+        System.out.println("f4 = " + f4);
 
+        System.out.println("\n=== OPERACIONES ===");
         System.out.println("Suma = " + f1.sumar(f2));
         System.out.println("Resta = " + f1.restar(f2));
         System.out.println("Multiplicación = " + f1.multiplicar(f2));
         System.out.println("División = " + f1.dividir(f2));
 
+        System.out.println("\n=== UTILIDADES ===");
         System.out.println("Opuesta de f1 = " + f1.opuesta());
         System.out.println("Inversa de f2 = " + f2.inversa());
         System.out.println("Decimal de f1 = " + f1.aDecimal());
         System.out.println("¿f1 es cero? " + f1.esCero());
 
+        System.out.println("\n=== COMPARACIONES ===");
         System.out.println("compareTo entre f1 y f2 = " + f1.compareTo(f2));
         System.out.println("¿f1 < f2? " + f1.menorQue(f2));
         System.out.println("¿f1 > f2? " + f1.mayorQue(f2));
         System.out.println("¿f1 <= f2? " + f1.menorOIgualQue(f2));
         System.out.println("¿f1 >= f2? " + f1.mayorOIgualQue(f2));
 
+        System.out.println("\n=== EQUALS Y CLONE ===");
         System.out.println("¿f1 es igual a f4? " + f1.equals(f4));
         System.out.println("Copia de f1 = " + copia);
     }
