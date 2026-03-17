cc393edb659102d4c749cddb7b16e7217b299d0a
rodrigorgo1909-web <rodrigorgo1909@gmail.com>
2026-03-17 13:46:02 +0100

    ahora si integro en el codigo la ayuda proporcionada por el reto para calcular el maximo comun divisor que se usa para simplificar fracciones. esto lo añado usando el algoritmo de Euclides

--- entregas/Fraccion.java
+++ entregas/Fraccion.java
@@ -11,4 +11,13 @@ public class Fraccion {
         this.denominador = denominador;
     }
 
+    private int calcularMCD(int a, int b) {
+        while (b != 0) {
+            int temporal = b;
+            b = a % b;
+            a = temporal;
+        }
+        return a;
+    }
+
 }
