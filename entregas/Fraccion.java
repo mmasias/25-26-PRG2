2d150988fa50300e08d0d4e43a9beb70bc19b5ec
rodrigorgo1909-web <rodrigorgo1909@gmail.com>
2026-03-17 13:42:22 +0100

    añado un atributo de clase (this.numerador) y parametro (numerador) lluego this sirve para diferenciar entre ambos

--- entregas/Fraccion.java
+++ entregas/Fraccion.java
@@ -3,4 +3,9 @@ public class Fraccion {
     private int numerador;
     private int denominador;
 
+    public Fraccion(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
 }
