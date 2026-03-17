fadc43a296e5650fa9e58b8a573b3e89a890c4be
rodrigorgo1909-web <rodrigorgo1909@gmail.com>
2026-03-17 13:40:22 +0100

    añado la clase con sus atributos

--- entregas/Fraccion.java
+++ entregas/Fraccion.java
@@ -1,8 +1,6 @@
-int calcularMCD(int a, int b) {
-    while (b != 0) {
-        int temporal = b;
-        b = a % b;
-        a = temporal;
-    }
-    return a;
+public class Fraccion {
+
+    private int numerador;
+    private int denominador;
+
 }
