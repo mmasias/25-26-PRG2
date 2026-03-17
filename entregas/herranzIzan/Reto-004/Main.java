9a6c35130f85389ce471d61e2d3a985f04912d2f
izanherranz07 <izan.herranz@alumnos.uneatlantico.es>
2026-03-17 13:56:25 +0100

    Agrego la clase Main para demostrar el uso de Fraccion.
    crear Main.java, comprobar que las fracciones se crean bien y comprobar que se simplifican

---
+++ entregas/herranzIzan/Reto-004/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Fraccion f1 = new Fraccion(2, 4);
+        Fraccion f2 = new Fraccion(3, 5);
+        Fraccion f3 = new Fraccion(-6, -8);
+
+        System.out.println("f1 = " + f1);
+        System.out.println("f2 = " + f2);
+        System.out.println("f3 = " + f3);
+    }
+}
