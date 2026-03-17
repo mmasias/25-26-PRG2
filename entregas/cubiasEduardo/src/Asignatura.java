f0d20c0e866cf926473e84911a72ec99445ae7a5
Eduardo Rene Cubias Moran <eduardo.cubias@unealum.local>
2026-03-17 13:46:32 +0100

    feat: Cree constructor de la clase fraccion

--- entregas/cubiasEduardo/src/Asignatura.java
+++
@@ -1,4 +0,0 @@
-class Asignatura {
-    public Asignatura(String nombre, String codigo, int creditos, String[] idiomas)
-    https://m.ok.ru/video/11640632969775
-}
---
+++ entregas/cubiasEduardo/src/Fraccion.java
@@ -0,0 +1,22 @@
+class Fraccion {
+    private int numerador;
+    private int denominador;
+    
+    public Fraccion(int numerador, int denominador)
+    {
+        numerador = this.numerador;
+        denominador = this.denominador;
+    }
+     
+    public Fraccion(int numerador)
+    {
+        this(numerador, 1);
+    }
+
+    public Fraccion()
+    {
+        this(1,1);
+    }
+
+    
+}
