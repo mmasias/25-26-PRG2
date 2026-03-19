0fcb07deedae6d8caf29d87c3499465e8aa7528f
Asier Garrastazu <asier.gc20072gmail.com>
2026-03-19 22:28:54 +0100

    estructura inicial

---
+++ entregas/Fraccion.java
@@ -0,0 +1,12 @@
+package entregas;
+
+public class Fraccion {
+
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+}
