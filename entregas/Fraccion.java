557820c0ff13c10a51170807ad77bbb34b3a0114
Asier Garrastazu <asier.gc20072gmail.com>
2026-03-19 22:32:55 +0100

    añadir validación de denominador distinto de cero

--- entregas/Fraccion.java
+++ entregas/Fraccion.java
@@ -6,7 +6,11 @@ public class Fraccion {
     private int denominador;
 
     public Fraccion(int numerador, int denominador) {
-        this.numerador = numerador;
-        this.denominador = denominador;
+    if (denominador == 0) {
+        throw new IllegalArgumentException("El denominador no puede ser 0");
     }
+    this.numerador = numerador;
+    this.denominador = denominador;
+}
+
 }
