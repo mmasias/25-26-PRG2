3ddd34e2ba7e059b05002f57df797b4779586ddb
Fabian <dylanfabian2424@gmail.com>
2026-03-19 11:07:52 +0100

    Se implemento constructor por defecto, operaciones básicas de fracciones (sumar, restar, multiplicar, dividir) con simplificación

---
+++ evaluaciones/retos/reto004/Entrega/Fraccion.java
@@ -0,0 +1,64 @@
+package vistas.fraccion;
+
+public class Fraccion {
+
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        if (denominador == 0) throw new IllegalArgumentException("Denominador no puede ser 0");
+        this.numerador = numerador;
+        this.denominador = denominador;
+        simplificar();
+    }
+
+    public Fraccion (int numero){
+        this.numerador = numero;
+        this.denominador = 1;
+    } 
+
+    public Fraccion(Fraccion fraccion){
+        this.numerador = fraccion.numerador;
+        this.denominador = fraccion.denominador;
+    }
+
+    public Fraccion(){
+        this.numerador = 0;
+        this.denominador = 1;
+    }
+
+    public Fraccion clonar(){
+        return new Fraccion(this);
+    }
+
+    public void sumar (Fraccion fraccion){
+        this.numerador = this.numerador * fraccion.denominador + fraccion.numerador * this.denominador;
+        this.denominador = this.denominador * fraccion.denominador;
+        simplificar();
+    }
+
+    public void restar(Fraccion fraccion){
+        this.numerador = this.numerador * fraccion.denominador - fraccion.numerador * this.denominador;
+        this.denominador = this.denominador * fraccion.denominador;
+        simplificar();
+    }
+
+    public void multiplicar(Fraccion fraccion){
+        this.numerador *= fraccion.numerador;
+        this.denominador *= fraccion.denominador;
+        simplificar();
+    }
+
+    public void dividir(Fraccion fraccion){
+        if (fraccion.numerador == 0) throw new IllegalArgumentException("División por 0");
+        this.numerador *= fraccion.denominador;
+        this.denominador *= fraccion.numerador;
+        simplificar();
+    }
+
+
+
+
+
+
+}
