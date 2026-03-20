f4b1499795f1401cdde059ad5d0d690434198c93
felipe <felipe@gmail.como>
2026-03-20 11:12:30 +0100

    agrego construcctores encadenados

--- entregas/luisHoyos/Fraccion.java
+++ entregas/luisHoyos/Fraccion.java
@@ -3,4 +3,24 @@ package vistas.fraccion;
 class Fraccion {
     private int numerador;
     private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        assert denominador != 0;
+
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    public Fraccion (int numerador) {
+        this(numerador, 1);
+    }
+
+    public Fraccion() {
+        this(0, 1);
+    }
+
+    public Fraccion(Fraccion fraccion) {
+        this(fraccion.numerador, fraccion.denominador);
+    }
+
 }
