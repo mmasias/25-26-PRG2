5e320cc3e806ccb93b252386f7103ace75c51e6b
Eduardo Rene Cubias Moran <eduardo.cubias@unealum.local>
2026-03-20 10:30:15 +0100

    fix: resolution errors

--- entregas/cubiasEduardo/src/Fraccion.java
+++ entregas/cubiasEduardo/src/Fraccion.java
@@ -2,6 +2,21 @@ class Fraccion {
     private int numerador;
     private int denominador;
 
+    public Fraccion(int numerador, int denominador) {
+        assert denominador != 0 : "Denominador invalido";
+        this.numerador = numerador;
+        this.denominador = denominador;
+        this.simplificar();
+    }
+
+    public Fraccion(int numerador) {
+        this(numerador, 1);
+    }
+
+    public Fraccion() {
+        this(1, 1);
+    }
+
     private int calcularMCD(int a, int b) {
         while (b != 0) {
             int temporal = b;
@@ -35,7 +50,7 @@ class Fraccion {
 
     private void sumarFracciones(int numerador, int denominador) {
         assert denominador != 0 : "Denominador invalido";
-        
+
         if (denominador == this.denominador) {
             this.numerador = this.numerador + numerador;
         } else {
@@ -45,75 +60,28 @@ class Fraccion {
         this.simplificar();
     }
 
-    public Fraccion(int numerador, int denominador) {
-        assert denominador != 0 : "Denominador invalido";
-        this.numerador = numerador;
-        this.denominador = denominador;
-        this.simplificar();
-    }
-     
-    public Fraccion(int numerador) {
-        this(numerador, 1);
-    }
-
-    public Fraccion() {
-        this(1, 1);
-    }
-
-    public void sumar(int numerador, int denominador) {
-        sumarFracciones(numerador, denominador);
-    }
-    
-    public void sumar(int numerador) {
-        sumarFracciones(numerador, 1);
-    }
-
     public void sumar(Fraccion otraFraccion) {
         sumarFracciones(otraFraccion.numerador, otraFraccion.denominador);
-    }
-
-    public void restar(int numerador, int denominador) {
-        sumarFracciones(-numerador, denominador);
-    }
-
-    public void restar(int numerador) {
-        sumarFracciones(-numerador, 1);
     }
 
     public void restar(Fraccion otraFraccion) {
         sumarFracciones(-otraFraccion.numerador, otraFraccion.denominador);
     }
 
-    public void multiplicar(int numerador, int denominador) {
-        assert denominador != 0 : "Denominador invalido";
-        this.numerador = this.numerador * numerador;
-        this.denominador = this.denominador * denominador;
-        simplificar();
-    }
-
-    public void multiplicar(int numerador) {
-        multiplicar(numerador, 1);
-    }
-
     public void multiplicar(Fraccion otraFraccion) {
-        multiplicar(otraFraccion.numerador, otraFraccion.denominador);
-    }
-
-    public void dividir(int numerador, int denominador) {
         assert denominador != 0 : "Denominador invalido";
-        assert numerador != 0 : "No se puede dividir por cero"; 
-        
-        this.numerador = this.numerador * denominador;
-        this.denominador = this.denominador * numerador;
+        numerador = numerador * otraFraccion.numerador;
+        denominador = denominador * otraFraccion.denominador;
         simplificar();
-    }
-
-    public void dividir(int numerador) {
-        dividir(numerador, 1);
     }
 
     public void dividir(Fraccion otraFraccion) {
-        dividir(otraFraccion.numerador, otraFraccion.denominador);
+        assert denominador != 0 : "Denominador invalido";
+        assert numerador != 0 : "No se puede dividir por cero";
+
+        numerador = numerador * otraFraccion.denominador;
+        denominador = denominador * otraFraccion.numerador;
+        simplificar();
     }
 
     public Fraccion opuesta() {
@@ -141,9 +109,6 @@ class Fraccion {
     }
 
     public boolean equals(Fraccion otraFraccion) {
-        if (otraFraccion == null) {
-            return false;
-        }
         int producto1 = numerador * otraFraccion.denominador;
         int producto2 = otraFraccion.numerador * denominador;
         return producto1 == producto2;
@@ -154,11 +119,9 @@ class Fraccion {
     }
 
     public String compareTo(Fraccion otraFraccion) {
-        assert otraFraccion != null : "La fraccion es null";
-        
         int producto1 = numerador * otraFraccion.denominador;
         int producto2 = otraFraccion.numerador * denominador;
-        
+
         if (producto1 < producto2) {
             return "Menor";
         }
