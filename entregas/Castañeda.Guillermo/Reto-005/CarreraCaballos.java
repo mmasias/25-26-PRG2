87b5b7e683e45e54fd00986cb109f5a78f93336f
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-04-24 11:24:29 +0200

    Creo turno aleatorio

--- entregas/Castañeda.Guillermo/Reto-005/CarreraCaballos.java
+++ entregas/Castañeda.Guillermo/Reto-005/CarreraCaballos.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class CarreraCaballos {
     public static void main(String[] args){
         Carrera carrera = new Carrera();
@@ -7,10 +9,17 @@ public class CarreraCaballos {
 
 class Carrera {
     private int meta = 40;
+    private Random r = new Random();
 
     public void jugar() {
         Caballo c1 = new Caballo(1);
         Caballo c2 = new Caballo(2);
+
+        int turno = r.nextInt(2);
+
+        if (turno == 0) c1.avanzar(r.nextInt(3));
+        else c2.avanzar(r.nextInt(3));
+
         mostrar(c1, c2);
     }
 
@@ -40,6 +49,10 @@ class Caballo {
     public Caballo(int numero) {
         this.numero = numero;
         this.posicion = 0;
+    }
+
+    public void avanzar(int pasos) {
+        posicion += pasos;
     }
 
     public int getPosicion() {
