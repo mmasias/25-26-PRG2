934061e289aff44f2755d2d1ffd7758f3ef8e22a
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-04-24 11:11:11 +0200

    Dibujo pista y caballos

--- entregas/Castañeda.Guillermo/Reto-005/CarreraCaballos.java
+++ entregas/Castañeda.Guillermo/Reto-005/CarreraCaballos.java
@@ -6,10 +6,30 @@ public class CarreraCaballos {
 }
 
 class Carrera {
+    private int meta = 40;
+
     public void jugar() {
         Caballo c1 = new Caballo(1);
-        c1.avanzar(2);
-        System.out.println(c1.getPosicion());
+        Caballo c2 = new Caballo(2);
+        mostrar(c1, c2);
+    }
+
+    private void mostrar(Caballo c1, Caballo c2) {
+        System.out.println("----------------------------------------------");
+
+        for (int i = 0; i < meta; i++) {
+            if (i == c1.getPosicion()) System.out.print(";-;'");
+            else System.out.print(" ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < meta; i++) {
+            if (i == c2.getPosicion()) System.out.print(";-;'");
+            else System.out.print(" ");
+        }
+        System.out.println();
+
+        System.out.println("----------------------------------------------");
     }
 }
 
@@ -20,10 +40,6 @@ class Caballo {
     public Caballo(int numero) {
         this.numero = numero;
         this.posicion = 0;
-    }
-
-    public void avanzar(int pasos) {
-        posicion += pasos;
     }
 
     public int getPosicion() {
