f83decbe0bea0d464a394c922f1d4fa425c32e44
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-04-24 12:04:42 +0200

    Creación metodo jugar
    Creo el método jugar y creo otra vez la consola y los caballos y creo la clase pista

---
+++ entregas/Carrera.java
@@ -0,0 +1,11 @@
+public class Carrera {
+  public void jugar() {
+        Console console = new Console();
+        Caballos caballo1 = new Caballos(console);
+        Caballos caballo2 = new Caballos(console);
+        Pista pista = new Pista();
+  }
+
+
+
+}
