1f7c6ec7e0bbf08003f144f2863b47b0b6487e0c
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-04-24 10:57:30 +0200

    Añado caballo

--- entregas/Castañeda.Guillermo/Reto-005/CarreraCaballos.java
+++ entregas/Castañeda.Guillermo/Reto-005/CarreraCaballos.java
@@ -7,6 +7,20 @@ public class CarreraCaballos {
 
 class Carrera {
     public void jugar() {
-        System.out.println("Carrera iniciada");
+        Caballo c1 = new Caballo(1);
+        Caballo c2 = new Caballo(2);
+        System.out.println("Caballos: " + c1.getNumero() + ", " + c2.getNumero());
+    }
+}
+
+class Caballo {
+    private int numero;
+
+    public Caballo(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
     }
 }
