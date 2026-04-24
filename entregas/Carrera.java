26ca30e8fecb26262566bc9674eae2388314a89b
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-04-24 21:40:09 +0200

    Creacion bucle do while
    Creo un bucle do while, donde este no pare hasta que uno de los caballos no llegue ha meta, o lleguen los dos a la vez, y despues incluir lo que haremos dentro de este bucle

--- entregas/Carrera.java
+++ entregas/Carrera.java
@@ -1,9 +1,21 @@
 public class Carrera {
   public void jugar() {
-        Console console = new Console();
-        Caballos caballo1 = new Caballos(console);
-        Caballos caballo2 = new Caballos(console);
-        Pista pista = new Pista();
+      Console console = new Console();
+      Caballos caballo1 = new Caballos(console);
+      Caballos caballo2 = new Caballos(console);
+      Pista pista = new Pista();
+    
+      do {
+        pista.mostrar();
+        caballo1.movimientoCaballo();
+        caballo2.movimientoCaballo();
+        console.pause(1);
+        console.cleanScreen();
+
+      } while (!caballo1.haTerminado() && !caballo2.haTerminado());
+
+
+    
   }
 
 
