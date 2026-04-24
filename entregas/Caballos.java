38abef8ca92f8d485ea4f112ca043d582553b9c3
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-04-24 11:46:27 +0200

    Creacion Atributos clase Caballos
    Creación de los Atributos que utilizaremos en la clase Caballos

---
+++ entregas/Caballos.java
@@ -0,0 +1,9 @@
+public class Caballos {
+    
+    private String caballo = ";-;'";
+    private String fichasMovimiento = " ";
+    private int posiciónCaballo = 0;
+    private String avance = "";
+    private Console console;
+
+}
