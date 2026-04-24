c131e258dfe80e2be8a2bb70b21746c29c0b17f5
Felipe <felipehoyos2008@gmail.com>
2026-04-24 13:02:55 +0200

    feat: agregar atributos Pista, Caballo[], Turno y Console a la clase Carrera

--- entregas/FelipeHoyos/src/Carrera.java
+++ entregas/FelipeHoyos/src/Carrera.java
@@ -1,9 +1,14 @@
 public class Carrera {
 
+    private Pista pista;
+    private Caballo[] caballos;
+    private Turno turno;
+    private Console console;
+
     public Carrera() {}
 
     public void jugar()
 
-    
+
 }
     
