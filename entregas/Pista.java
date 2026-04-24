da989eed77e5d1b7701828b73c28e1fee94fefaa
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-04-24 11:59:44 +0200

    Creación clase Pista
    Creo la clase Pista, y le asigno sus respectivos atributos

---
+++ entregas/Pista.java
@@ -0,0 +1,8 @@
+public class Pista {
+    private String Separador = "---+-----------+---------+---------+---------+---------+-----------+";
+    private String Camino1 = "[1]";
+    private String Camino2 = "[2]";
+    private int turno = 0;
+
+
+}
