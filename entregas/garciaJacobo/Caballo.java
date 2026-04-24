689dc1e5459668dcd4a4c83d5ead5a15c4f866d3
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-04-24 11:54:52 +0200

    feat: clase carrera implementada! Subimos las Clases auxiliares que iremos abordando sucesivamente.

---
+++ entregas/garciaJacobo/Caballo.java
@@ -0,0 +1,12 @@
+public class Caballo {
+
+    public Caballo(int i) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void mover(Pista pista) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mover'");
+    }
+
+}
---
+++ entregas/garciaJacobo/Pista.java
@@ -0,0 +1,22 @@
+public class Pista {
+
+    public Pista(int i) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void mostrar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+    }
+
+    public boolean hayGanadores() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hayGanadores'");
+    }
+
+    public void mostrarResultadoFinal() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrarResultadoFinal'");
+    }
+
+}
