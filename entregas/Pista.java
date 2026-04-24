0dbdd73c7c38c4d54d21050518e1e1cfeda2b484
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-04-24 12:01:19 +0200

    Creación objetos
    Creo la consola y los dos caballos de la carrera

--- entregas/Pista.java
+++ entregas/Pista.java
@@ -4,5 +4,10 @@ public class Pista {
     private String Camino2 = "[2]";
     private int turno = 0;
 
+    public void mostrar() {
+        Console console = new Console();
+        Caballos caballo1 = new Caballos(console);
+        Caballos caballo2 = new Caballos(console);
+
 
 }
