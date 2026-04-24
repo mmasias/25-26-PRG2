bea1ab91dcb4ab0e06cfa43f74fd668cfb7f9517
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-04-24 21:43:30 +0200

    Determinación ganadores
    Determinamos con un if, else, el ganador de la carrera, y en caso de empate, determinar que ha sido un empate

--- entregas/Carrera.java
+++ entregas/Carrera.java
@@ -1,5 +1,6 @@
 public class Carrera {
-  public void jugar() {
+  
+    public void jugar() {
       Console console = new Console();
       Caballos caballo1 = new Caballos(console);
       Caballos caballo2 = new Caballos(console);
@@ -14,10 +15,14 @@ public class Carrera {
 
       } while (!caballo1.haTerminado() && !caballo2.haTerminado());
 
-
-    
+      if (caballo1.haTerminado() && caballo2.haTerminado()) {
+          console.writeln("Han llegado los dos caballos, ¡Ha sido un empate!");
+      } else {
+          if (caballo1.haTerminado()) {
+              console.writeln("El caballo 1 ha ganado!!");
+          } else {
+              console.writeln("El caballo 2 ha ganado!!");
+          }
+      }
   }
-
-
-
 }
