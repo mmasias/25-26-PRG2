19e168844b74a8b9e48169d448cf4f168374bc3c
Felipe <felipehoyos2008@gmail.com>
2026-04-24 13:13:55 +0200

    feat: agregar clase Caballo con métodospublicos

---
+++ entregas/FelipeHoyos/src/Caballo.java
@@ -0,0 +1,12 @@
+public class Caballo {
+    
+    public Caballo(int numero)
+
+    public int numero()
+
+    public int posicion()
+
+    public void avanzar()
+
+    public boolean haLlegado(Pista pista)
+}
