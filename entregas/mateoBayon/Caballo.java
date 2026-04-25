b5678db73e2fa62c7e743dd075ef6b620858c7c9
mateobayon8-hub <mateobayon8@gmail.com>
2026-04-25 16:01:28 +0200

    feat: Implementar clase Caballo con lógica de movimiento en la carrera.

---
+++ entregas/mateoBayon/Caballo.java
@@ -0,0 +1,27 @@
+class Juego {
+    public static void main(String[] args){
+        Carrera carrera = new Carrera();
+        carrera.jugar();
+    }
+}
+
+public class Caballo {
+    private int posicion;
+    private String modelo = ";-;'";
+
+    public Caballo(){
+        this.posicion = 0;
+    }
+
+    public void caballoAvanza(){
+        this.posicion += (int)(Math.random() * 3);
+    }
+
+    public int obtenerPosicion() {
+        return posicion;
+    }
+
+    public String obtenerModelo() {
+        return modelo;
+    }
+}
--- entregas/mateoBayon/Juego.java
+++
@@ -1,6 +0,0 @@
-class Juego {
-    public static void main(String[] args){
-        Carrera carrera = new Carrera();
-        carrera.jugar();
-    }
-}
