229de7f8e6a3b9afaead4df2e8d375c5e8edcf2b
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-04-26 12:33:23 +0200

    "Eliminar espacios no necesarios  y habilitar y habilitar pausa entre turnos mediante la tecla enter"

--- entregas/garciaJacobo/Carrera.java
+++ entregas/garciaJacobo/Carrera.java
@@ -1,23 +1,24 @@
 public class Carrera {
-
     private Pista pista;
     private Caballo[] caballos;
+    private Console console;
 
     public Carrera() {
         pista = new Pista(40);
         caballos = new Caballo[2];
         caballos[0] = new Caballo(1);
         caballos[1] = new Caballo(2);
+        console = new Console();
     }
-    
+
     public void jugar() {
         do {
-            pista.mostrar();
+            pista.mostrar(caballos);
             for (int i = 0; i < 2; i++) {
                 caballos[i].mover(pista);
             }
-        } while (!pista.hayGanadores());
-        pista.mostrarResultadoFinal();
+            console.readString("");
+        } while (!pista.hayGanadores(caballos));
+        pista.mostrarResultadoFinal(caballos);
     }
-
 }
