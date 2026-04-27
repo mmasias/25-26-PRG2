7225d6ff4145e6ff493083914de3411aadab7a93
lucasmorales-crypto <lucas.morales@alumnos.uneatlantico.es>
2026-04-27 22:10:48 +0100

    Implemento la clase Caballo

---
+++ entregas/moralesLucas/reto-005/Caballo.java
@@ -0,0 +1,23 @@
+public class Caballo {
+
+    private int numero;
+    private int posicion;
+
+    public Caballo(int dorsal) {
+        numero = dorsal;
+        posicion = 0;
+    }
+
+    public void mover() {
+        int avance = (int)(Math.random() * 3);
+        posicion += avance;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+}
