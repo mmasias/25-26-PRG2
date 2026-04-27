7447577f069691bf3af6230df74371abcc4fc0c5
alejandroportilla-create <alejandro.portilla@alumnos.uneatlantico.es>
2026-04-27 20:24:02 +0200

    clase pista implementada

--- entregas/portillaAlejandro/reto-005/Pista.java
+++ entregas/portillaAlejandro/reto-005/Pista.java
@@ -1,5 +1,32 @@
 
-
 public class Pista {
-    
+
+    private int longitud;
+
+    public Pista(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void mostrar(Caballo caballo1, Caballo caballo2) {
+
+        System.out.println("----------------------------------------------");
+
+        for (int i = 0; i <= longitud; i++) {
+
+            if (i == caballo1.getPosicion()) {
+                System.out.print(";-;'");
+            } else if (i == caballo2.getPosicion()) {
+                System.out.print(";-;'");
+            } else {
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+        System.out.println("----------------------------------------------");
+    }
 }
