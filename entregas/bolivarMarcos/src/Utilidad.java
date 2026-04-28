bf632a4613e54077fd2d5c82844d7b8c6748a219
Cyber <marcosbolivar2007@gmail.com>
2026-04-28 10:29:14 +0200

    Completada clase Utilidad

--- entregas/bolivarMarcos/src/Utilidad.java
+++ entregas/bolivarMarcos/src/Utilidad.java
@@ -1,3 +1,14 @@
 public class Utilidad {
-    
+    public static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void pausar(int milisegundo){
+        try {
+            Thread.sleep(milisegundos);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
