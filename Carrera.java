5488b05a13234828a13a9c8d40d799e992694ae6
javier.turcios <javier.turcios@ct.uneatlantico.es>
2026-04-28 11:10:25 +0200

    Añadir avance aleatorio de caballos

--- Carrera.java
+++ Carrera.java
@@ -1,7 +1,14 @@
+import java.util.Random;
+
 class Carrera {
 
     private static final int LONGITUD = 40;
     private int[] posiciones = new int[2];
+    private Random azar = new Random();
+
+    private void avanzar(int caballo) {
+        posiciones[caballo] += azar.nextInt(3);
+    }
 
     private void mostrarPista() {
         System.out.println("--------------------------------------------");
