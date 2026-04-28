e871e5245a9b9572945cdb3d94ac559a85ea88b7
javier.turcios <javier.turcios@ct.uneatlantico.es>
2026-04-28 11:10:00 +0200

    Añadir método mostrarPista

--- Carrera.java
+++ Carrera.java
@@ -3,6 +3,14 @@ class Carrera {
     private static final int LONGITUD = 40;
     private int[] posiciones = new int[2];
 
+    private void mostrarPista() {
+        System.out.println("--------------------------------------------");
+        for (int i = 0; i < posiciones.length; i++) {
+            System.out.println(" ".repeat(posiciones[i]) + ";-;'");
+        }
+        System.out.println("--------------------------------------------");
+    }
+
     void jugar() {
     }
 }
