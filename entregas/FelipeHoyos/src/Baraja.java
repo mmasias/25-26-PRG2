c972cb32691fc3c998e0d0bc1ebd64c739099974
luis.hoyos <luis.hoyos>
2026-05-08 11:30:04 +0200

    feat: agregar atributos cartas y ultima en la clase Baraja y ajustar el método hayCartas

--- entregas/FelipeHoyos/src/Baraja.java
+++ entregas/FelipeHoyos/src/Baraja.java
@@ -1,10 +1,19 @@
 class Baraja {
+
+    private Carta[] cartas;
+    private int ultima;
     
     public Baraja() {
+        ultima = 0;
+        cartas = new Carta[52];
+
+        this.generar();
+        this.mezclar();
 
     }
 
     public boolean hayCartas() {
+        return ultima < 0;
     }
 
     public Carta sacar() {
