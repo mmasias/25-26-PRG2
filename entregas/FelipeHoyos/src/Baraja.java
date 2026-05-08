1d7070695cb480638ffebfcb2733da2c4941beb9
luis.hoyos <luis.hoyos>
2026-05-08 11:36:08 +0200

    refactor: corregir lógica en el método hayCartas para verificar correctamente si hay cartas disponibles

--- entregas/FelipeHoyos/src/Baraja.java
+++ entregas/FelipeHoyos/src/Baraja.java
@@ -20,8 +20,22 @@ class Baraja {
         }
     }
 
+    private void poner(Carta carta) {
+        assert carta != null;
+
+        cartas[ultima] = carta;
+        ultima = ultima + 1;
+    }
+
+    private void mezclar() {
+        for (int i = 0; i < ultima; i++) {
+            int posicion = (int) (Math.random() * ultima);
+            this.intercambiar(i, posicion);
+        }
+    }
+
     public boolean hayCartas() {
-        return ultima < 0;
+        return ultima > 0;
     }
 
     public Carta sacar() {
