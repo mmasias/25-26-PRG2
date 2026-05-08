3429773ac03f87e38f64f9dea2a8f67e3cd21a36
Guayito_developer <106986590+Eduardo-Cubiasss@users.noreply.github.com>
2026-05-08 09:52:37 +0000

    feat: add class initial BlackJack

--- entregas/cubiasEduardo/src/BlackJack.java
+++ entregas/cubiasEduardo/src/BlackJack.java
@@ -1,16 +1,25 @@
 class BlackJack {
+    private Mano mano;
 
-    public Klondike() {
-    
+    public BlackJack() {
+        mano = new Mano();
+
     }
 
     private void jugar() {
         Menu menu = new Menu();
-        
+        Dealer dealer = new Dealer(mano);
 
+        boolean estaJugando = true;
+        boolean haGanado = true;
+
+       do {
+            
+        } while (estaJugando && !haGanado);
+        console.writeln((!haGanado ? "NO " : "") + "GANASTE");
     }
 
     public static void main(String[] args) {
-        new Klondike().jugar();
+        new BlackJack().jugar();
     }
 }
