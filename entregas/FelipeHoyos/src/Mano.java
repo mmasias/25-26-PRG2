c517e64ee62dd8ef5b6bff6981807d1c1d7d2c40
luis.hoyos <luis.hoyos>
2026-05-08 11:44:20 +0200

    feat: agregar atributos y lógica inicial en la clase Mano para gestionar cartas

--- entregas/FelipeHoyos/src/Mano.java
+++ entregas/FelipeHoyos/src/Mano.java
@@ -1,10 +1,20 @@
 class Mano {
+
+    private Carta[] cartas;
+    private int tamaño;
+    private Console console;
     
     public Mano() {
+        cartas = new Carta[52];
+        tamaño = 0;
+        console = new Console();
     }
 
     public void recibir(Carta carta) {
         assert carta != null;
+
+        cartas[tamaño] = carta;
+        tamaño = tamaño + 1;
     }
 
     public int puntaje() {
