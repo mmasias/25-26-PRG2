1d0bf8befa2155b95565bf372cbd9ab69915bbea
luis.hoyos <luis.hoyos>
2026-05-08 11:02:08 +0200

    feat: agregar atributos a la clase Blackjack
    le he puesto los metoos privados para ir pensando bien en las demas clases q manejara este

--- entregas/FelipeHoyos/src/Blackjack.java
+++ entregas/FelipeHoyos/src/Blackjack.java
@@ -1,4 +1,11 @@
 public class Blackjack {
+
+    private Baraja baraja;
+    private Descarte descarte;
+    private Mano mano;
+    private Menu menu;
+    private Console console;
+
     public Blackjack() {
     }
 }
