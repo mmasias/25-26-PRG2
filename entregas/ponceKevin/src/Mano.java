6d0104f520ee936f2f6ea3e9e4e007fe50f9ca7d
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-05-08 09:31:18 +0000

    feat: implementar métodos para calcular y mostrar el puntaje de la mano

--- entregas/ponceKevin/src/Mano.java
+++ entregas/ponceKevin/src/Mano.java
@@ -1,18 +1,30 @@
 public class Mano {
     Carta[] cartas = new Carta[52];
     int indiceSiguienteCarta = 0;
-    int puntaje = 0;
 
     Mano() {
     }
 
     public void agregarCarta(Carta carta) {
+        cartas[indiceSiguienteCarta] = carta;
+        indiceSiguienteCarta++;
     }
 
     public void mostrarMano() {
+        System.out.println("puntaje: " + calcularValor()+"de 21 \n");
+        System.out.println("Tu mano:");
+        for (int i = 0; i < indiceSiguienteCarta; i++) {
+            cartas[i].mostrarCarta();
+        }
     }
 
     public int calcularValor() {
-        return 0;
+        int valorTotal = 0;
+        for (int i = 0; i < indiceSiguienteCarta; i++) {
+            valorTotal += valorCarta(cartas[i]);
+        }
+        return valorTotal;
     }
+
+ 
 }
