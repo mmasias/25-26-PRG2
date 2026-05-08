64b879e52a4e277d85d05b90a27e51a1b674c1ba
luis.hoyos <luis.hoyos>
2026-05-08 11:42:17 +0200

    feat: implementar métodos para gestionar cartas en la clase Descarte

--- entregas/FelipeHoyos/src/Descarte.java
+++ entregas/FelipeHoyos/src/Descarte.java
@@ -9,11 +9,33 @@ class Descarte {
     }
 
     public boolean hayCartas() {
-        
+        return tamaño > 0;
     }
 
     public void recibir (Carta carta) {
         assert carta != null;
+
+        cartas[tamaño] = carta;
+        tamaño = tamaño + 1;
+    }
+
+    private Carta sacar() {
+        assert this.hayCartas();
+
+        tamaño = tamaño - 1;
+
+        Carta carta = cartas[tamaño];
+        cartas[tamaño] = null;
+
+        return carta;
+    }
+
+    public void moverA(Mano mano) {
+        assert mano != null;
+
+        if(this.hayCartas()) {
+            mano.recibir(this.sacar());
+        }
     }
 
 
