ec4a9aeb8e7ee76ad80dea6da380507a528857f4
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-05-08 09:26:53 +0000

    feat: agregar clase Mano con estructura básica y métodos iniciales

---
+++ entregas/ponceKevin/src/Mano.java
@@ -0,0 +1,18 @@
+public class Mano {
+    Carta[] cartas = new Carta[52];
+    int indiceSiguienteCarta = 0;
+    int puntaje = 0;
+
+    Mano() {
+    }
+
+    public void agregarCarta(Carta carta) {
+    }
+
+    public void mostrarMano() {
+    }
+
+    public int calcularValor() {
+        return 0;
+    }
+}
