4d6f35b7c26f1d213551489796701c3ab0662c3c
luis.hoyos <luis.hoyos>
2026-05-08 11:28:29 +0200

    feat: agregar métodos hayCartas, sacar y moverA en la clase Baraja
    defino metdos publicos para poder comenzar con la primera vista de baraja

--- entregas/FelipeHoyos/src/Baraja.java
+++ entregas/FelipeHoyos/src/Baraja.java
@@ -1,3 +1,15 @@
 class Baraja {
     
+    public Baraja() {
+
+    }
+
+    public boolean hayCartas() {
+    }
+
+    public Carta sacar() {
+    }
+
+    public void moverA(Descarte descarte) {
+    }
 }
