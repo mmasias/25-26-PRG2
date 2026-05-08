fa5c9f345a2db29e186a355dca8d2c0ff5503134
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-05-08 09:17:42 +0000

    feat: corregir inicialización de variables en la clase BlackJack

--- entregas/rafaelKaterine/src/BlackJack.java
+++ entregas/rafaelKaterine/src/BlackJack.java
@@ -2,14 +2,21 @@ class BlackJack {
 
     private Baraja baraja;
     private Carta[] misCartas;
-    private int numMisCartas;
+    private int puntiaje;
     private Console console;
 
     public BlackJack() {
         baraja = new Baraja();
         console = new Console();
         misCartas = new Carta[52]; 
-        numMisCartas = 0;
+        puntiaje = 0;
     }
+    private void iniciarNuevaPartida() {
+            this.baraja = new Baraja();
+            this.puntiaje = 0;
+            this.recibirCarta();
+            this.recibirCarta();
+        }
 
+    
 }
