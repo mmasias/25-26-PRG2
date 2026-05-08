dadfad0d895fb018e46fd1d05592fd3072fd56fc
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-05-08 09:23:36 +0000

    refactor: mejorar la estructura del código en la clase Baraja y implementa algoriitmo de mezcla

--- entregas/ponceKevin/src/Baraja.java
+++ entregas/ponceKevin/src/Baraja.java
@@ -1,8 +1,11 @@
+import java.util.Random;
+
 public class Baraja {
     Carta[] cartas = new Carta[52];
+
     Baraja() {
         inicializarBaraja();
-
+        mezclar();
     }
 
     public Carta sacarCarta() {
@@ -10,18 +13,24 @@ public class Baraja {
     }
 
     private void mezclar() {
-
-    }
-
-private void inicializarBaraja() {
-    String[] palos = {"Corazones", "Diamantes", "Tréboles", "Picas"};
-    String[] valores = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-    int index = 0;
-    for (String palo : palos) {
-        for (String valor : valores) {
-            cartas[index] = new Carta(palo, valor);
-            index++;
+        Random random = new java.util.Random();
+        for (int i = cartas.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            Carta temp = cartas[i];
+            cartas[i] = cartas[j];
+            cartas[j] = temp;
         }
     }
-}
+
+    private void inicializarBaraja() {
+        String[] palos = { "Corazones", "Diamantes", "Tréboles", "Picas" };
+        String[] valores = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        int index = 0;
+        for (String palo : palos) {
+            for (String valor : valores) {
+                cartas[index] = new Carta(palo, valor);
+                index++;
+            }
+        }
+    }
 }
