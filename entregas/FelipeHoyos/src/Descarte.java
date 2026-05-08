73322b456838e3de70c9dd026747f81ea6a055de
luis.hoyos <luis.hoyos>
2026-05-08 11:38:52 +0200

    feat: agregar clase Descarte con métodos iniciales para gestionar cartas
    defiinicion de metodos publicos de descarteee

---
+++ entregas/FelipeHoyos/src/Descarte.java
@@ -0,0 +1,14 @@
+class Descarte {
+    public Descarte() {
+    }
+
+    public boolean hayCartas() {
+        
+    }
+
+    public void recibir (Carta carta) {
+        assert carta != null;
+    }
+
+    
+}
