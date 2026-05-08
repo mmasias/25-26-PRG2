69c2d3f836d3cddf8f6d9279c549f0b308f4e638
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-05-08 09:15:13 +0000

    feat: inicializar variables en el constructor de la clase BlackJack

--- entregas/rafaelKaterine/src/BlackJack.java
+++ entregas/rafaelKaterine/src/BlackJack.java
@@ -1,10 +1,15 @@
-Class BlackJack {
+class BlackJack {
 
     private Baraja baraja;
-    
+    private Carta[] misCartas;
+    private int numMisCartas;
+    private Console console;
 
     public BlackJack() {
         baraja = new Baraja();
+        console = new Console();
+        misCartas = new Carta[52]; 
+        numMisCartas = 0;
     }
 
 }
