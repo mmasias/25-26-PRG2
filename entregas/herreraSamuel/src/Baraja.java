552a1c7a1b559cce91d1c08187a74d684214b799
samyx <samyxelita@gmail.com>
2026-05-09 09:13:11 +0200

    Atributos privados y constructor de clase baraja

--- entregas/herreraSamuel/src/Baraja.java
+++ entregas/herreraSamuel/src/Baraja.java
@@ -1,4 +1,13 @@
 public class Baraja {
+    private Carta[] cartas;
+    private int siguiente;
+
+    public Baraja() {
+        cartas = new Carta[52];
+        siguiente = 0;
+        this.inicializarCartas(); 
+        this.barajar();           
+    }
 
     public Carta repartir() {
      
