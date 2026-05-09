82af135f2fd78e1d6895725d258b2dc1296c34cf
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-05-09 11:10:17 +0200

    Creo la funcionalidad de repartir 2 cartas y sumar

--- entregas/Castañeda.Guillermo/Reto-006/Blackjack.java
+++ entregas/Castañeda.Guillermo/Reto-006/Blackjack.java
@@ -9,9 +9,23 @@ public class Blackjack {
         String[] valores = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
         String[] palos = {"♥", "♣", "♦", "♠"};
 
-        int posicion = aleatorio.nextInt(13);
-        String mano = "[" + valores[posicion] + " " + palos[aleatorio.nextInt(4)] + "]";
-        
-        System.out.println("Tu primera carta es: " + mano);
+        String mano = "";
+        int puntos = 0;
+
+        for (int i = 0; i < 2; i++) {
+            int posicion = aleatorio.nextInt(13);
+            String valorCarta = valores[posicion];
+            mano += "[" + valorCarta + " " + palos[aleatorio.nextInt(4)] + "]";
+            
+            if (valorCarta.equals("A")) {
+                puntos += 11;
+            } else if (posicion >= 9) {
+                puntos += 10;
+            } else {
+                puntos += Integer.parseInt(valorCarta);
+            }
+        }
+
+        System.out.println("Mano: " + mano + " - Puntaje: " + puntos);
     }
 }
