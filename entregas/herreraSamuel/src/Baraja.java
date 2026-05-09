42ebb78a60005e03fe9e538a9000545189394209
samyx <samyxelita@gmail.com>
2026-05-09 13:43:55 +0200

    feat: implementar clase Baraja completa
    Explicación: Siguiendo HOOD, la Baraja encapsula la creación y mezcla del mazo. Al delegar la creación de 52 objetos Carta a su propio constructor, garantizamos que el sistema Blackjack siempre trabaje con un estado inicial válido y aleatorio, desacoplando la generación de datos de la lógica del juego.

--- entregas/herreraSamuel/src/Baraja.java
+++ entregas/herreraSamuel/src/Baraja.java
@@ -9,7 +9,32 @@ public class Baraja {
         this.barajar();           
     }
 
+    private void inicializarCartas() {
+        String[] palos = {"♥", "♣", "♦", "♠"};
+        String[] rangos = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        int indice = 0;
+
+        for (int i = 0; i < palos.length; i++) {
+            for (int j = 0; j < rangos.length; j++) {
+                cartas[indice] = new Carta(palos[i], rangos[j]);
+                indice++;
+            }
+        }
+    }
+
+    private void barajar() {
+        for (int i = 0; i < cartas.length; i++) {
+            int posicionAzar = (int) (Math.random() * cartas.length);
+            Carta temporal = cartas[i];
+            cartas[i] = cartas[posicionAzar];
+            cartas[posicionAzar] = temporal;
+        }
+    }
+
     public Carta repartir() {
+        Carta cartaAEntregar = cartas[siguiente];
+        siguiente++;
+        return cartaAEntregar;
      
     }
 
