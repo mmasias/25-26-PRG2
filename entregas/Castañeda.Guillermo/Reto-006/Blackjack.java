faa619f6231797765b331a334f2a64e1912b9d6e
Guillermo-Castaneda <guillermo.castaneda@alumnos.uneatlantico.es>
2026-05-09 11:23:09 +0200

    Creo opción de pedir cartas y As doble valor

--- entregas/Castañeda.Guillermo/Reto-006/Blackjack.java
+++ entregas/Castañeda.Guillermo/Reto-006/Blackjack.java
@@ -11,21 +11,40 @@ public class Blackjack {
 
         String mano = "";
         int puntos = 0;
+        int contadorAses = 0;
+        int opcionUsuario = 1;
 
         for (int i = 0; i < 2; i++) {
             int posicion = aleatorio.nextInt(13);
             String valorCarta = valores[posicion];
             mano += "[" + valorCarta + " " + palos[aleatorio.nextInt(4)] + "]";
-            
-            if (valorCarta.equals("A")) {
-                puntos += 11;
-            } else if (posicion >= 9) {
-                puntos += 10;
-            } else {
-                puntos += Integer.parseInt(valorCarta);
-            }
+            if (valorCarta.equals("A")) { contadorAses++; puntos += 11; }
+            else if (posicion >= 9) { puntos += 10; }
+            else { puntos += Integer.parseInt(valorCarta); }
         }
 
-        System.out.println("Mano: " + mano + " - Puntaje: " + puntos);
+        while (opcionUsuario == 1) {
+            while (puntos > 21 && contadorAses > 0) {
+                puntos -= 10;
+                contadorAses--;
+            }
+
+            String estado = (puntos == 21) ? "Ganó" : (puntos > 21) ? "Perdió" : "Sigue jugando";
+            System.out.println("Mano: " + mano + " - Puntaje: " + puntos + " ==> " + estado);
+
+            if (puntos >= 21) break;
+
+            System.out.println("1. Pedir\n2. Salir");
+            opcionUsuario = teclado.nextInt();
+
+            if (opcionUsuario == 1) {
+                int posicion = aleatorio.nextInt(13);
+                String valorCarta = valores[posicion];
+                mano += "[" + valorCarta + " " + palos[aleatorio.nextInt(4)] + "]";
+                if (valorCarta.equals("A")) { contadorAses++; puntos += 11; }
+                else if (posicion >= 9) { puntos += 10; }
+                else { puntos += Integer.parseInt(valorCarta); }
+            }
+        }
     }
 }
