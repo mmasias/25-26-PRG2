93fffda607ec3c0bdec67f0999b4bea416a8ec9c
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-05-10 13:43:27 +0200

    feat: implementar bucle principal de juego, con reparto inicial y menú de opciones

--- entregas/garciaJacobo/Blackjack.java
+++ entregas/garciaJacobo/Blackjack.java
@@ -2,13 +2,40 @@
 public class Blackjack {
     private Baraja baraja;
     private Mano mano;
+    private Console console;
 
     public Blackjack() {
         baraja = new Baraja();
         mano = new Mano();
+        console = new Console();
     }
 
     private void jugar() {
+        boolean hayaGanado;
+        boolean estaJugando = true;
+
+        do {
+            if (mano.estaVacia()) {
+                Carta carta1 = baraja.sacar();
+                Carta carta2 = baraja.sacar();
+                mano.añadir(carta1);
+                mano.añadir(carta2);
+            }
+            int opcion = console.readInt();
+            switch (opcion) {
+                case 1 -> {
+                    Carta nueva = baraja.sacar();
+                    nueva.voltear();
+                    mano.añadir(nueva);
+                }
+
+                case 2 -> mano.nuevaMano();
+                case 3 -> estaJugando = false;
+            }
+            hayaGanado = mano.haGanado();
+            mano.mostrar();
+            mano.getPuntuacion();
+        } while (!hayaGanado && estaJugando);
 
     }
 
