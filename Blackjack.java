7000a2d423d6b932d86d8dd85afaa49caa1862f6
Asier Garrastazu <asier.gc20072gmail.com>
2026-05-11 11:51:39 +0200

    Creamos las clases necesarias

---
+++ Blackjack.java
@@ -0,0 +1,17 @@
+
+class Blackjack {
+    
+    private Baraja baraja;
+    private Mano mano;
+    private Console console;
+    private Menu menu;
+
+    public Blackjack() {
+        baraja = new Baraja();
+        mano = new Mano();
+        console = new Console();
+        menu = new Menu();
+    }
+
+
+}
