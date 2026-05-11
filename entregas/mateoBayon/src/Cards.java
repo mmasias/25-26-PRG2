af7c24fe019ec30f9feffcec14a1e761b92ed4fd
mateobayon8-hub <mateobayon8@gmail.com>
2026-05-11 18:08:27 +0200

    feat: Añado clase Cards con constructor para rank y suit.

--- entregas/mateoBayon/src/Cards.java
+++ entregas/mateoBayon/src/Cards.java
@@ -1,4 +1,9 @@
 public class Cards {
     private String rank;
     private String suit;
+
+    public Cards(String rank, String suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
 }
