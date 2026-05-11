1178db95997ceff9d41b1074e5b4368dc796af32
mateobayon8-hub <mateobayon8@gmail.com>
2026-05-11 19:26:20 +0200

    feat: Creo método dealCard para repartir una carta al jugador o una nula si ya no quedan cartas en el mazo.

--- entregas/mateoBayon/src/Deck.java
+++ entregas/mateoBayon/src/Deck.java
@@ -1,15 +1,16 @@
 public class Deck {
-    private Cards[] cards;
+    private Card[] cards;
+    private int nextCardIndex = 0;
 
     public Deck() {
         String [] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
         String [] suits = {"♥", "♦", "♣", "♠"};
-        this.cards = new Cards[52];
+        this.cards = new Card[52];
 
         int index = 0;
         for(int r = 0; r < ranks.length; r++){
             for(int s = 0; s < suits.length; s++){
-                this.cards[index] = new Cards(ranks[r], suits[s]);
+                this.cards[index] = new Card(ranks[r], suits[s]);
                 index++;
             }
         }
@@ -18,9 +19,19 @@ public class Deck {
     public void shuffleCards() {
         for(int i = 0; i < cards.length; i++){
             int randomIndex = (int) (Math.random()) * 52;
-            Cards temp = cards[i];
+            Card temp = cards[i];
             cards[i] = cards[randomIndex];
             cards[randomIndex] = temp;
+        }
+    }
+
+    public Card dealCard() {
+        if(nextCardIndex < cards.length){
+            Card card = this.cards[nextCardIndex];
+            nextCardIndex++;
+            return card;
+        } else {
+            return null;
         }
     }
 }
