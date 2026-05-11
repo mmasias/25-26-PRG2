2ec497bd12879fe026cf8855bb436855484c833c
eduardo.cubias <eduardo.cubiasm@gmail.com>
2026-05-11 21:13:39 +0200

    feat: add class BlackJack and add logic initial

--- entregas/cubiasEduardo/src/BlackJack.java
+++ entregas/cubiasEduardo/src/BlackJack.java
@@ -1,22 +1,50 @@
 class BlackJack {
+
+    private Baraja baraja;
     private Mano mano;
+    private Console console;
 
     public BlackJack() {
+        baraja = new Baraja();
         mano = new Mano();
-
+        console = new Console();
     }
 
     private void jugar() {
         Menu menu = new Menu();
-        Dealer dealer = new Dealer(mano);
+        prepararPartida();
 
-        boolean estaJugando = true;
-        boolean haGanado = true;
+        boolean salir = false;
+        do {
+            mostrarTapete();
+            int opcion = menu.pedirOpcion();
+            switch (opcion) {
+                case 1 -> pedirCarta();
+                case 2 -> prepararPartida();
+                case 3 -> salir = true;
+            }
+        } while (!salir);
+    }
 
-       do {
-            
-        } while (estaJugando && !haGanado);
-        console.writeln((!haGanado ? "NO " : "") + "GANASTE");
+    private void prepararPartida() {
+        baraja = new Baraja();
+        mano = new Mano();
+        mano.poner(baraja.sacar());
+        mano.poner(baraja.sacar());
+    }
+
+    private void pedirCarta() {
+        if (mano.puedeSeguir()) {
+            mano.poner(baraja.sacar());
+        } else {
+            console.writeln("No puedes pedir más cartas.");
+        }
+    }
+
+    private void mostrarTapete() {
+        console.writeln("--------------------");
+        mano.mostrar();
+        console.writeln("--------------------");
     }
 
     public static void main(String[] args) {
