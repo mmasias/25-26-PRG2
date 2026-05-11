b02fc3cec31302e90086c157ba0fd61c8ad3e44d
eduardo.cubias <eduardo.cubiasm@gmail.com>
2026-05-11 21:22:01 +0200

    feat: add class Menu

--- entregas/cubiasEduardo/src/Menu.java
+++ entregas/cubiasEduardo/src/Menu.java
@@ -2,23 +2,18 @@ public class Menu {
 
     private Console console;
 
-    public Menu(){
+    public Menu() {
         console = new Console();
     }
 
     public void mostrar() {
-        console.writeln("1. Mover Baraja a Descarte");
-        console.writeln("2. Mover Descarte a Palo");
-        console.writeln("3. Mover Descarte a Columna");
-        console.writeln("4. Mover de Palo a Columna");
-        console.writeln("5. Mover de Columna a Palo");
-        console.writeln("6. Mover de Columna a Columna");
-        console.writeln("7. Voltear Descarte en Baraja");
-        console.writeln("8. Voltear carta de Columna");
-        console.writeln("9. Salir");
+        console.writeln("1. Pedir");
+        console.writeln("2. Empezar de nuevo");
+        console.writeln("3. Salir");
     }
 
     public int pedirOpcion() {
-        return console.readInt();
+        mostrar();
+        return console.readInt("Opción: ");
     }
 }
