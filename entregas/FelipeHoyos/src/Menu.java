29a5c2a8e9bab4397531ef106671711c9f4db07b
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-05-12 11:38:51 +0200

    feat: inicializar consola en la clase Menu y agregar opciones al método pedirOpcion

--- entregas/FelipeHoyos/src/Menu.java
+++ entregas/FelipeHoyos/src/Menu.java
@@ -1,8 +1,15 @@
 class Menu {
+    private Console console;
+
     public Menu() {
-        
+        console = new Console();    
     }
     public int pedirOpcion() {
+        console.writeln("1. Pedir");
+        console.writeln("2. Empezar de nuevo");
+        console.writeln("3. Salir");
+
+        return console.readInt("Opcion: ");
 
     }
 }
