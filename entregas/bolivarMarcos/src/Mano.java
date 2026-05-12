90544dd748728f0245dff5c581a6e18478bedbce
Cyber <marcosbolivar2007@gmail.com>
2026-05-12 11:24:20 +0200

    Completada calse mano

--- entregas/bolivarMarcos/src/Mano.java
+++ entregas/bolivarMarcos/src/Mano.java
@@ -11,4 +11,47 @@ public class Mano {
         this.contador = 0;
         this.console = new Console();
     }
+    public void añadir(Carta carta) {
+        if (this.contador < this.cartas.length) {
+            this.cartas[this.contador] = carta;
+            this.contador++;
+        }
+    }
+
+    public int sumaTotal() {
+        int suma = 0;
+        int ases = 0;
+
+        for (int i = 0; i < contador; i++) {
+            suma += cartas[i].valor21();
+            if (cartas[i].esAs()) {
+                ases++;
+            }
+        }
+
+
+        while (ases > 0 && suma + 10 <= 21) {
+            suma += 10;
+            ases--;
+        }
+
+        return suma;
+    }
+
+    public boolean tiene21() {
+        return this.sumaTotal() == 21;
+    }
+
+    public boolean esMenora21() {
+        return this.sumaTotal() < 21;
+    }
+
+    public void mostrar() {
+        this.console.write("Mano: ");
+        for (int i = 0; i < this.contador; i++) {
+            this.cartas[i].mostrar();
+            this.console.write(" ");
+        }
+        this.console.writeln("| Suma: " + this.sumaTotal());
+    }
 }
