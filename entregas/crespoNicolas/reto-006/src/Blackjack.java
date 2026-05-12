cf846931a549745c45ee25bf6d26c7f33349b778
nicolascrespo-dotcom <nicolas.crespo@alumnos.uneatlantico.es>
2026-05-12 11:23:40 +0200

    Implement game loop in jugar method

--- entregas/crespoNicolas/reto-006/src/Blackjack.java
+++ entregas/crespoNicolas/reto-006/src/Blackjack.java
@@ -1,10 +1,37 @@
-public class Blackjack {
-    
-    private Baraja baraja;
+private Baraja baraja;
     private Jugador jugador;
     private Console console;
+    private boolean jugando;
 
     public Blackjack () {}
 
     public void jugar () {}
+    public void jugar () {
+        
+        do {
+            
+            this.repartirCartasIniciales();
+            
+            switch (jugando) {
+                case 1 -> jugador.pedirCartas ();
+                case 2 -> jugador.mostrarResultadoFinal();
+                    break;
+            
+                default:
+                    break;
+            }
+
+        } while (jugando);
+
+    }
+
+    private void prepararPartida () {}
+
+    private void repartirCartasIniciales (
+        
+    ) {}
+
+    private void gestionarTurno () {}
+
+    private void mostrarResultadoFinal () {}
 }
