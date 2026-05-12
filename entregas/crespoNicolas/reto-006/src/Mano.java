79fe3afdd94c578e767f832ae9ea65620a402d08
nicolascrespo-dotcom <nicolas.crespo@alumnos.uneatlantico.es>
2026-05-12 11:29:00 +0200

    Implement methods for card hand management

--- entregas/crespoNicolas/reto-006/src/Mano.java
+++ entregas/crespoNicolas/reto-006/src/Mano.java
@@ -1,17 +1,55 @@
 public class Mano {
-    
-    private Carta [] cartas;
+    private Carta[] cartas;
     private int contador;
+    private Console console;
 
-    public Mano () {}
+    public Mano() {
+        this.cartas = new Carta[21];  
+        this.contador = 0;
+        this.console = new Console();
+    }
 
-    public void añadir (Carta carta) {}
+    public void añadir(Carta carta) {
+        if (this.contador < this.cartas.length) {
+            this.cartas[this.contador] = carta;
+            this.contador++;
+        }
+    }
 
-    public int contar () {}
+    public int sumaTotal() {
+        int suma = 0;
+        int ases = 0;
 
-    public boolean tiene21 () {}
+        for (int i = 0; i < contador; i++) {
+            suma += cartas[i].valor21();
+            if (cartas[i].esAs()) {
+                ases++;
+            }
+        }
 
-    public boolean esMenora21 () {}
 
-    public void mostrar () {}
+        while (ases > 0 && suma + 10 <= 21) {
+            suma += 10;
+            ases--;
+        }
+
+        return suma;
+    }
+
+    public boolean tiene21() {
+        return this.sumaTotal() == 21;
+    }
+
+    public boolean esMenora21() {
+        return this.sumaTotal() < 21;
+    }
+
+    public void mostrar() {
+        this.console.write("Mano: ");
+        for (int i = 0; i < this.contador; i++) {
+            this.cartas[i].mostrar();
+            this.console.write(" ");
+        }
+        this.console.writeln("| Suma: " + this.sumaTotal());
+    }
 }
