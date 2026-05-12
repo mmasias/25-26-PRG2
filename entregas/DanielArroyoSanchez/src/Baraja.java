3f77951a72e11130a3a900ffa7a3da819bfe2da9
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-05-12 09:45:49 +0200

    Creación métodos en Baraja
    Creación metodos finales en Baraja

--- entregas/DanielArroyoSanchez/src/Baraja.java
+++ entregas/DanielArroyoSanchez/src/Baraja.java
@@ -15,4 +15,49 @@ public class Baraja {
         this.mezclar();
         console = new Console();
     }
+
+    public void mezclar() {
+        
+        for (int i = 0; i < 1000; i++) {
+            int origen = (int) (Math.random() * 52);
+            int destino = (int) (Math.random() * 52);
+            Carta carta = cartas[origen];
+            cartas[origen] = cartas[destino];
+            cartas[destino] = carta;
+        }
+    }
+
+    public void poner(Carta carta) {
+        cartas[ultima] = carta;
+        ultima++;
+    }
+
+    public void mostrar() {
+        console.write("BARAJA: ");
+        if (vacia()) {
+            console.writeln("La baraja está vacía!!!");
+        } else {
+            Carta carta = cima();
+            carta.mostrar();
+            console.writeln();
+        }
+    }
+
+    private Carta cima() {
+        return cartas[ultima - 1];
+    }
+
+    public Carta sacar() {
+        assert !vacia();
+        ultima--;
+        return cartas[ultima];
+    }
+
+    public void darCarta(Mano mano) {
+
+    }
+
+    public boolean vacia() {
+        return ultima == 0;
+    }
 }
