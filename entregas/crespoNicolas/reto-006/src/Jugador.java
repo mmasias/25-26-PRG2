ac6bdd1b651204a152eed5dbb4bcee156a010c80
nicolascrespo-dotcom <nicolas.crespo@alumnos.uneatlantico.es>
2026-05-12 11:22:25 +0200

    Implement agarrar method in Jugador class
    Added 'agarrar' method to Jugador class.

--- entregas/crespoNicolas/reto-006/src/Jugador.java
+++ entregas/crespoNicolas/reto-006/src/Jugador.java
@@ -1,12 +1,12 @@
-public class Jugador {
-    
-    private Mano mano; 
-
-    public Jugador () {}
 
     public int consultarPuntaje () {}
 
     public void recibirCartas () {}
 
     public boolean seHaPasado () {}
+
+    public void agarrar(Carta carta) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'agarrar'");
+    }
 }
