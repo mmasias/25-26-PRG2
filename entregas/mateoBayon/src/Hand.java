f122da239787da62d27fef623a86d5926ff079e4
mateobayon8-hub <mateobayon8@gmail.com>
2026-05-12 11:21:31 +0200

    feat: Defino método totalPoints para calcular los puntos totales.

--- entregas/mateoBayon/src/Hand.java
+++ entregas/mateoBayon/src/Hand.java
@@ -1,9 +1,16 @@
 public class Hand {
     private Card[] myCards;
-    private int numCards;
+    public int numCards = 0;
 
     public Hand() {
         this.myCards = new Card[2];
         this.numCards = 0;
+    }
+
+    public int totalPoints() {
+        for(int i = 0; i < myCards.length; i++) {
+            numCards += myCards[i].getValueRank();
+        }
+        return numCards;
     }
 }
