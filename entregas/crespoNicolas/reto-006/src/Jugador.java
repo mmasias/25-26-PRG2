453676f14edb7c772f88c52962001fb0c7b498d7
nicolascrespo-dotcom <nicolas.crespo@alumnos.uneatlantico.es>
2026-05-12 11:19:11 +0200

    vista publica jugador

--- entregas/crespoNicolas/reto-006/src/Jugador.java
+++ entregas/crespoNicolas/reto-006/src/Jugador.java
@@ -1,3 +1,12 @@
 public class Jugador {
     
+    private Mano mano; 
+
+    public Jugador () {}
+
+    public int consultarPuntaje () {}
+
+    public void recibirCartas () {}
+
+    public boolean seHaPasado () {}
 }
