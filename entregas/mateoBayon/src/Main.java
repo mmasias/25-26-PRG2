077921a5d5614607e032c4adeb6ea165ead9c703
mateobayon8-hub <mateobayon8@gmail.com>
2026-05-12 11:57:44 +0200

    feat: Bucle de juego.

--- entregas/mateoBayon/src/Main.java
+++ entregas/mateoBayon/src/Main.java
@@ -5,5 +5,16 @@ public class Main {
         Scanner playerInput = new Scanner(System.in);
         Deck deck = new Deck();
         Hand playerHand = new Hand();
+
+        while(!playerHand.playerWon()){
+            System.out.println("Do you want to get a card? (Y/N)");
+            String answer = playerInput.nextLine();
+            if(answer == "Y"){
+                playerHand.getCard(deck.dealCard());
+            } else {
+                System.out.println("You have " + playerHand.totalPoints() + " points.");
+            }
+            playerHand.playerWon();
+        }
     }
 }
