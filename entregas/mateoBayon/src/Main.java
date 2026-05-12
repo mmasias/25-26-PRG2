178b63500c9a5cd757cd2df8d046298f9831eb65
mateobayon8-hub <mateobayon8@gmail.com>
2026-05-12 11:40:09 +0200

    feat: Defino clases Deck y Hand, y creo instancia de Scanner para entrada del usuario

--- entregas/mateoBayon/src/Main.java
+++ entregas/mateoBayon/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner playerInput = new Scanner(System.in);
+        Deck deck = new Deck();
+        Hand playerHand = new Hand();
     }
 }
