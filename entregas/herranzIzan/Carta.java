b6c5375e783e8dfff38098a649d61d24c27e6dd6
izanherranz07 <izan.herranz@alumnos.uneatlantico.es>
2026-05-12 11:31:56 +0200

    feat: añadir clase Carta con palo, número y representación visual

---
+++ entregas/herranzIzan/Carta.java
@@ -0,0 +1,17 @@
+public class Carta {
+    private int palo;
+    private int numero;
+    private final String[] PALOS = { "♠️", "♣️", "♦️", "♥️" };
+    private final String[] NUMEROS = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    private Console console;
+
+    public Carta(int palo, int numero) {
+        this.palo = palo;
+        this.numero = numero;
+        console = new Console();
+    }
+
+    public void mostrar() {
+        console.write("[" + NUMEROS[this.numero] + " " + PALOS[this.palo] + "]");
+    }
+}
