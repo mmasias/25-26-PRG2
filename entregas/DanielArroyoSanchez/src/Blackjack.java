a4441857dd34c8b6a80c1b071e0491302d826ef6
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-05-12 09:45:07 +0200

    Creación metodos en Blackjack
    Creación metodos en Blackjack, jugar, mostrarTapete donde muestro la mano del jugador, y sus opciones, y método de pedirCarta y finalmente el main

--- entregas/DanielArroyoSanchez/src/Blackjack.java
+++ entregas/DanielArroyoSanchez/src/Blackjack.java
@@ -11,3 +11,62 @@ public class Blackjack {
         dealer = new Dealer();
 
     }
+
+    private void jugar() {
+        boolean estaJugando = true;
+
+        dealer.repartirInicial(mano);
+        mano.tieneBlackjack();
+
+        do {
+            console.cleanScreen();
+            this.mostrarTapete();
+            switch (menu.pedirOpcion()) {
+                case 1:
+                    this.pedirCarta();
+                    break;
+                case 2:
+                    dealer.reiniciar();
+                    this.mano = new Mano();
+                    dealer.repartirInicial(mano);
+                    break;
+                case 3:
+                    estaJugando = false;
+                    break;
+            }
+        } while (estaJugando);
+
+    }
+
+    private void mostrarTapete() {
+        this.linea();
+        console.write("Mano: ");
+        mano.mostrar();
+        this.linea();
+        menu.mostrar();
+        this.linea();
+
+    }
+
+    private void linea() {
+        console.writeln("------------------------------");
+    }
+
+    private void pedirCarta() {
+        if (!mano.haGanado() && !mano.seHaPasado()) {
+            dealer.darCarta(mano);
+        } else if (mano.haGanado()) {
+            console.writeln("Has Ganado!!, empieza una nueva partida o sal.");
+        } else if (mano.seHaPasado()) {
+            console.writeln("Has Perdido!!, empieza una nueva partida o sal.");
+
+        } else {
+            console.writeln("Has perdido, vuelve a empezar o sal.");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Blackjack().jugar();
+    }
+
+}
