082834b6e3d79195a157a405abbaa7157211b3d0
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-05-12 09:35:28 +0200

    Creación metodos en Mano
    Creo los métodos de poner y getPuntaje, donde vamos viendo el puntaje total de nuestra mano

--- entregas/DanielArroyoSanchez/src/Mano.java
+++ entregas/DanielArroyoSanchez/src/Mano.java
@@ -9,3 +9,26 @@ public class Mano {
         this.ultima = 0;
         this.console = new Console();
     }
+
+    public void poner(Carta carta) {
+        if (ultima < cartas.length) {
+            cartas[ultima] = carta;
+            ultima++;
+        }
+    }
+
+    public int getPuntaje() {
+        int total = 0;
+        int ases = 0;
+        for (int i = 0; i < ultima; i++) {
+            total += cartas[i].getValor();
+            if (cartas[i].esAs()) {
+                ases++;
+            }
+        }
+        while (total > 21 && ases > 0) {
+            total -= 10;
+            ases--;
+        }
+        return total;
+    }
