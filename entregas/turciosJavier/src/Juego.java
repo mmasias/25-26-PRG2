25e4c3a61aea53dbbb2a0bdc92cf821ecbc0465b
javier.turcios <javier.turcios@ct.uneatlantico.es>
2026-05-12 11:13:48 +0200

    Feat: bucle para volver a jugar

--- entregas/turciosJavier/src/Juego.java
+++ entregas/turciosJavier/src/Juego.java
@@ -12,18 +12,35 @@ class Juego {
     }
 
     public void jugar() {
-        rondaBlackjack();
+        boolean jugando = true;
+
+        while (jugando) {
+            mano.limpiar();
+            rondaBlackjack();
+
+            System.out.println("1. Empezar de nuevo");
+            System.out.println("2. Salir");
+            System.out.print("Opción: ");
+            int opcion = scanner.nextInt();
+
+            if (opcion == 2) {
+                jugando = false;
+                System.out.println("¡Hasta luego!");
+            }
+        }
     }
 
     private void rondaBlackjack() {
-        mano.limpiar();
         mano.agregarCarta(baraja.repartir());
         mano.agregarCarta(baraja.repartir());
-        System.out.println(mano);
 
         boolean sigue = true;
 
         while (sigue && mano.estaBuena()) {
+            System.out.println("--------------------");
+            System.out.println(mano);
+            System.out.println("==> Sigue jugando");
+            System.out.println("--------------------");
             System.out.println("1. Pedir");
             System.out.println("2. Plantarse");
             System.out.print("Opción: ");
@@ -31,18 +48,20 @@ class Juego {
 
             if (opcion == 1) {
                 mano.agregarCarta(baraja.repartir());
-                System.out.println(mano);
             } else {
                 sigue = false;
             }
         }
 
+        System.out.println("--------------------");
+        System.out.println(mano);
         if (mano.calcularPuntos() == 21) {
-            System.out.println("¡Ganó! BLACKJACK");
+            System.out.println("==> ¡Ganó! BLACKJACK");
         } else if (mano.estaBuena()) {
-            System.out.println("Se plantó con " + mano.calcularPuntos());
+            System.out.println("==> Se plantó con " + mano.calcularPuntos());
         } else {
-            System.out.println("Perdió (se pasó de 21)");
+            System.out.println("==> Perdió (se pasó de 21)");
         }
+        System.out.println("--------------------");
     }
 }
