86bbace85ad23638b4b39b8ed3d22b8507bf28aa
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-05-12 11:13:26 +0200

    feat: agregar método valorBlackJack en la clase Carta para calcular el valor en el juego

--- entregas/FelipeHoyos/src/Carta.java
+++ entregas/FelipeHoyos/src/Carta.java
@@ -3,6 +3,16 @@ class Carta {
     public Carta(int palo, int numero) {
     }
     
+    public int valorBlackJack() {
+        if (numero > 10) {
+        return 10;
+     } 
+
+        return numero;
+
+    }
+    
+
 
 
 }
