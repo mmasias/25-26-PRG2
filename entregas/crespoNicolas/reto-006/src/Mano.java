433cf271d9f9984088c49eb7b3ba6a69e8f2e2b6
nicolascrespo-dotcom <nicolas.crespo@alumnos.uneatlantico.es>
2026-05-12 11:18:19 +0200

    Add properties and methods to Mano class

--- entregas/crespoNicolas/reto-006/src/Mano.java
+++ entregas/crespoNicolas/reto-006/src/Mano.java
@@ -1,3 +1,17 @@
 public class Mano {
     
+    private Carta [] cartas;
+    private int contador;
+
+    public Mano () {}
+
+    public void añadir (Carta carta) {}
+
+    public int contar () {}
+
+    public boolean tiene21 () {}
+
+    public boolean esMenora21 () {}
+
+    public void mostrar () {}
 }
