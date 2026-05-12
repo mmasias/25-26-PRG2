ceeb5ed1031ee88c9f62e19ccd0b62ab00ccce88
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-05-12 11:18:13 +0200

    feat: corregir tipo de retorno en el método numeroComoTexto de la clase Carta

--- entregas/FelipeHoyos/src/Carta.java
+++ entregas/FelipeHoyos/src/Carta.java
@@ -30,7 +30,24 @@ class Carta {
     }
 
 
+    private string numeroComoTexto() {
+        switch (numero) {
+            case 1:
+                return "A";
+            case 11:
+                return "J";
+            case 12:
+                return "Q";
+            case 13:
+                return "K";
+            
+        }
 
+        return "" + numero;
+
+    }
+
+    
 
 
 }
